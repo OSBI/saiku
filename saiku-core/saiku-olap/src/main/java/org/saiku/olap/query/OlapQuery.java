7f4ea5d1c2e64647a9039bac615dfe8d9fac3f2a
Paul Stoellberger <pstoellberger@gmail.com>
2010-11-07 13:33:41 +0000

    new axis

--- saiku-core/saiku-olap/src/main/java/org/saiku/olap/query/OlapQuery.java
+++ saiku-core/saiku-olap/src/main/java/org/saiku/olap/query/OlapQuery.java
@@ -1,11 +1,29 @@
 package org.saiku.olap.query;
 
+import java.util.Map;
+
+import org.olap4j.Axis;
 import org.olap4j.query.Query;
+import org.olap4j.query.QueryAxis;
 
 public class OlapQuery {
 
 	Query query;
+	
 	public OlapQuery(Query query) {
 		this.query = query;
 	}
+	
+	public void pivot() {
+		this.query.swapAxes();
+	}
+	
+	public Map<Axis, QueryAxis> getAxes() {
+		return this.query.getAxes();
+	}
+	
+	public QueryAxis getAxis(Axis axis) {
+		return this.query.getAxis(axis);
+	}
+	
 }
