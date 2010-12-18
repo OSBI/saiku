c8cb43b705d05abcd91b46ebfd03ea23544214b3
Paul Stoellberger <pstoellberger@gmail.com>
2010-12-19 00:15:11 +0100

    add copyrights

--- saiku-core/saiku-olap/src/main/java/org/saiku/olap/util/HierarchicalCellSetFormatter.java
+++ saiku-core/saiku-olap/src/main/java/org/saiku/olap/util/HierarchicalCellSetFormatter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Thomas Barber
+ * Copyright (C) 2010 Paul Stoellberger
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
