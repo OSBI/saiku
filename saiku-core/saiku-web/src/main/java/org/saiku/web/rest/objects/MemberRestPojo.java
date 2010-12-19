0b160bf213068c6ae4cc661e02258ff02d14422c
Paul Stoellberger <pstoellberger@gmail.com>
2010-12-19 04:35:34 +0100

    Revert "add copyrights"
    This reverts commit 94cac614a9158f0f1d75466bd2bc78051770c4b5.

--- saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/MemberRestPojo.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/MemberRestPojo.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2010 Paul Stoellberger
- *
- * This program is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the Free 
- * Software Foundation; either version 2 of the License, or (at your option) 
- * any later version.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
- *
- */
 package org.saiku.web.rest.objects;
 
 import javax.xml.bind.annotation.XmlAccessType;
