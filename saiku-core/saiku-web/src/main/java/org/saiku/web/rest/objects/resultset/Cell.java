43bf4f2261fb6bbbdf166afb8f4cbd624f3d0ae2
Paul Stoellberger <p.stoellberger@gmail.com>
2011-02-01 13:22:17 +0000

    remove props from cell

--- saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/resultset/Cell.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/resultset/Cell.java
@@ -1,13 +1,12 @@
 package org.saiku.web.rest.objects.resultset;
 
-import java.util.Properties;
 
 public class Cell  {
 
 	private String value;
 	private String type;
-	private Properties properties;
-	private Properties metaproperties;
+//	private Properties properties;
+//	private Properties metaproperties;
 
 	
 	public enum Type {
@@ -41,13 +40,13 @@ public class Cell  {
 		return value;
 	}
 
-	public Properties getProperties() {
-		return properties;
-	}
-
-	public Properties getMetaproperties() {
-		return metaproperties;
-	}
+//	public Properties getProperties() {
+//		return properties;
+//	}
+//
+//	public Properties getMetaproperties() {
+//		return metaproperties;
+//	}
 
 	public String getType() {
 		return type;
