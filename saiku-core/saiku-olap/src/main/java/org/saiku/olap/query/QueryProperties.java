696f76d492963cf1daf16afdf4414eee15b114fd
Paul Stoellberger <p.stoellberger@gmail.com>
2011-02-01 18:25:58 +0000

    add property for both axis NON EMPTY

--- saiku-core/saiku-olap/src/main/java/org/saiku/olap/query/QueryProperties.java
+++ saiku-core/saiku-olap/src/main/java/org/saiku/olap/query/QueryProperties.java
@@ -12,6 +12,7 @@ public class QueryProperties {
 		protected String value;
 		protected OlapQuery query;
 
+		public static final String KEY_NONEMPTY			= "saiku.olap.query.nonempty"; //$NON-NLS-1$
 		public static final String KEY_NONEMPTY_ROWS	= "saiku.olap.query.nonempty.rows"; //$NON-NLS-1$
 		public static final String KEY_NONEMPTY_COLUMNS = "saiku.olap.query.nonempty.columns"; //$NON-NLS-1$
 		public final String[] KEYS = new String[] { KEY_NONEMPTY_ROWS, KEY_NONEMPTY_COLUMNS };
@@ -63,6 +64,32 @@ public class QueryProperties {
 		}
 
 	}
+	
+	public class NonEmptyProperty extends QueryProperty {
+
+		public NonEmptyProperty(OlapQuery query, String key, String value) {
+			super(query,key,value);
+		}
+
+		@Override
+		public void handle() {
+			Boolean nonEmpty = Boolean.parseBoolean(NonEmptyProperty.this.value);
+			query.getAxis(Axis.ROWS).setNonEmpty(nonEmpty);
+			query.getAxis(Axis.COLUMNS).setNonEmpty(nonEmpty);
+		}
+
+		@Override
+		public Properties getProperties() {
+			String key = this.key;
+			String value = Boolean.toString(
+					query.getAxis(Axis.COLUMNS).isNonEmpty() &&
+					query.getAxis(Axis.ROWS).isNonEmpty());
+			Properties props = new Properties();
+			props.put(key, value);
+			return props;
+		}
+
+	}
 
 	public class NonEmptyColumnsProperty extends QueryProperty {
 
@@ -95,6 +122,9 @@ public class QueryProperties {
 			}
 			if (QueryProperty.KEY_NONEMPTY_COLUMNS.equals(key)) {
 				return q.new NonEmptyColumnsProperty(query, key, value);
+			}
+			if (QueryProperty.KEY_NONEMPTY.equals(key)) {
+				return q.new NonEmptyProperty(query, key, value);
 			}
 			return q.new DummyProperty(query, key, value);
 		}
