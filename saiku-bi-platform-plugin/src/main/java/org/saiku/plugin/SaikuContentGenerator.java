7eab88977cbbc329f45b752d93d61c08ed7b62ea
Paul Stoellberger <p.stoellberger@gmail.com>
2011-04-22 04:57:44 +0100

    plugin saving/loading actions

--- saiku-bi-platform-plugin/src/main/java/org/saiku/plugin/SaikuContentGenerator.java
+++ saiku-bi-platform-plugin/src/main/java/org/saiku/plugin/SaikuContentGenerator.java
@@ -20,20 +20,27 @@
 
 package org.saiku.plugin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.OutputStream;
 import java.security.InvalidParameterException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IParameterProvider;
+import org.pentaho.platform.api.engine.IPluginManager;
 import org.pentaho.platform.api.engine.IServiceManager;
+import org.pentaho.platform.api.engine.ISolutionFile;
 import org.pentaho.platform.api.repository.IContentItem;
 import org.pentaho.platform.api.repository.ISolutionRepository;
 import org.pentaho.platform.engine.core.solution.ActionInfo;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.services.solution.SimpleContentGenerator;
+import org.pentaho.platform.plugin.services.pluginmgr.PluginClassLoader;
 import org.pentaho.platform.util.messages.LocaleHelper;
+import org.saiku.plugin.util.PluginConfig;
 
 /**
  * A simple content generator that redirects .xpav content to PAT 
@@ -71,9 +78,41 @@ public class SaikuContentGenerator extends SimpleContentGenerator {
             LOG.error("Access to Repository has failed");
             throw new NullPointerException("Access to Repository has failed");
         }
-        if (repository.resourceExists(fullPath)) {
+     
+        if(requestParams.getStringParameter("query", null) != null) {
+        	String query = requestParams.getStringParameter("query", null); //$NON-NLS-1$
+        	String base = PentahoSystem.getApplicationContext().getSolutionRootPath();
+            String parentPath = ActionInfo.buildSolutionPath(solution, path, "");
+            ISolutionFile parentFile = repository.getSolutionFile(parentPath, ISolutionRepository.ACTION_CREATE);
+            String filePath = parentPath + ISolutionRepository.SEPARATOR + action;
+            ISolutionFile fileToSave = repository.getSolutionFile(fullPath, ISolutionRepository.ACTION_UPDATE);
+            
+            if (action != null && !action.endsWith(".saiku")) {
+            	action+= ".saiku";
+            }
+            
+            if (fileToSave != null || (!repository.resourceExists(filePath) && parentFile != null)) {
+                repository.publish(base, '/' + parentPath, action, query.getBytes() , true);
+                LOG.debug(PluginConfig.PLUGIN_NAME + " : Published " + solution + " / " + path + " / " + action );
+            } else {
+                throw new Exception("Error ocurred while saving query to solution repository");
+            }
+
+            
+            OutputStream out = null;
+            IContentItem contentItem = outputHandler.getOutputContentItem("response", "content", "", instanceId, getMimeType()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            if (contentItem == null) {
+                LOG.error("content item is null"); //$NON-NLS-1$
+                throw new NullPointerException("content item is null"); //$NON-NLS-1$
+            }
+
+            out = contentItem.getOutputStream(null);
+            contentItem.setMimeType("text/html");
+
+            out.flush();
+            out.close();
+        } else if (repository.resourceExists(fullPath)) {
             String doc = repository.getResourceAsString(fullPath);
-            LOG.error("#### DOCUMENT: " + doc);
             
             if (doc == null) {
                 LOG.error("Error retrieving saiku document from solution repository"); 
@@ -99,23 +138,6 @@ public class SaikuContentGenerator extends SimpleContentGenerator {
             }
 
         }
-        else if(requestParams.getStringParameter("query", null) != null) {
-            OutputStream out = null;
-
-            IContentItem contentItem = outputHandler.getOutputContentItem("response", "content", "", instanceId, getMimeType()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-
-            if (contentItem == null) {
-                LOG.error("content item is null"); //$NON-NLS-1$
-                throw new NullPointerException("content item is null"); //$NON-NLS-1$
-            }
-
-            out = contentItem.getOutputStream(null);
-            contentItem.setMimeType("text/html");
-            out.write("HALLO".getBytes());
-
-            out.flush();
-            out.close();
-        }
         else {
             super.createContent();
         }
@@ -126,7 +148,33 @@ public class SaikuContentGenerator extends SimpleContentGenerator {
     public void createContent(OutputStream out) throws Exception {
         try {
             StringBuilder html = new StringBuilder();
-            html.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\"    \"http://www.w3.org/TR/html4/strict.dtd\"><html>    <head>        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">        <title>Saiku - Next Generation Open Source Analytics</title>        <!-- Blueprint CSS -->        <link rel=\"stylesheet\" href=\"css/blueprint/src/reset.css\" type=\"text/css\" media=\"screen, projection\">        <link rel=\"stylesheet\" href=\"css/blueprint/src/typography.css\" type=\"text/css\" media=\"screen, projection\">        <link rel=\"stylesheet\" href=\"css/blueprint/src/forms.css\" type=\"text/css\" media=\"screen, projection\">        <!--[if lt IE 8]><link rel=\"stylesheet\" href=\"css/blueprint/src/ie.css\" type=\"text/css\" media=\"screen, projection\"><![endif]-->        <!-- Saiku CSS -->        <link rel=\"stylesheet\" href=\"css/saiku/src/styles.css\" type=\"text/css\" media=\"screen, projection\">        <link rel=\"shortcut icon\" href=\"favicon.ico\">        <!-- jQuery -->        <script src=\"js/jquery/src/jquery-1.4.4.min.js\" type=\"text/javascript\"></script>        <!-- jQuery Plugins -->        <script src=\"js/jquery/src/jquery-ui-1.8.9.custom.min.js\" type=\"text/javascript\"></script>        <script src=\"js/jquery/src/jquery.simplemodal.1.4.1.js\" type=\"text/javascript\"></script>        <script src=\"js/jquery/src/jquery.blockUI.2.3.7.js\" type=\"text/javascript\"></script>        <!-- Saiku -->        <script src=\"js/saiku/src/controller.js\" type=\"text/javascript\"></script>    </head>    <body>        <div id=\"header\" class=\"hide\">            <!-- toolbar -->            <div id=\"toolbar\" class=\"hide\">                <ul>                    <li><a id=\"add_tab\" href=\"#add_tab\" title=\"New query\" class=\"new_tab i18n\"></a></li>                    <li class=\"separator\">&nbsp;</li>                    <li><a id=\"open_query\" href=\"#open_query\" title=\"Open query\" class=\"open_query i18n\"></a></li>                    <li class=\"separator\">&nbsp;</li>                    <li><a id=\"logout\" href=\"#logout\" title=\"Logout\" class=\"logout i18n\"></a></li>                    <li><a id=\"about\" href=\"#about\" title=\"About\" class=\"about i18n\"></a></li>                    <li class=\"separator\">&nbsp;</li>                    <li><a id=\"issue_tracker\" href=\"#issue_tracker\" title=\"Issue Tracker\" class=\"bug i18n\"></a></li>                    <!--                    <li>                    	<select id=\"language-selector\">                    		<option value=\"en\">English</option>                    		<option value=\"it\">Italiano</option>                    		<option value=\"es\">Espa–ol</option>                    		<option value=\"cn\">Chinese (Mandarin)</option>                    	</select>                    </li> -->                </ul>                <h1 id=\"logo\"><a href=\"http://www.analytical-labs.com/\" title=\"Saiku - Next Generation Open Source Analytics\">Saiku</a></h1>            </div>            <!-- eof toolbar -->            <!-- tabs -->            <div id=\"tabs\" class=\"hide\"></div>        </div>        <!-- eof tabs -->        <!-- tab_panel -->        <div id=\"tab_panel\" class=\"hide\"></div>        <!-- eof tab_panel -->   DOCUMENT:" + document + "</body></html>");
+            final IPluginManager pluginManager = (IPluginManager) PentahoSystem.get(IPluginManager.class, PentahoSessionHolder.getSession());
+            final PluginClassLoader pluginClassloader = (PluginClassLoader)pluginManager.getClassLoader(PluginConfig.PLUGIN_NAME);
+            File pluginDir = pluginClassloader.getPluginDir();
+            File indexFile = new File(pluginDir,"ui/index.html");
+            LOG.error("#### PATH: "+ indexFile.getAbsolutePath());
+            FileReader fr = new FileReader(indexFile);
+            BufferedReader br = new BufferedReader(fr);
+            String inputLine;
+
+            while ((inputLine = br.readLine()) != null) {
+              html.append(inputLine);
+            }
+
+            if (document != null) {
+            	int bodyend = html.indexOf("</body>");
+            	document = document.replaceAll("\n", "").replaceAll("\r", "");
+            	if (bodyend >= 0) {
+            		html.append("\n");
+            		String snippet ="";
+            		snippet = "<script type=\"text/javascript\">\r\n";
+            		snippet += "QUERY = '" + document + "';\r\n";
+            		snippet += "</script>\r\n";
+            		html.insert(bodyend, snippet);
+            	}
+            	
+            }
+
             out.write(html.toString().getBytes(LocaleHelper.getSystemEncoding()));
 
         } catch (Exception e) {
