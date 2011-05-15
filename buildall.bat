d72196a7465cc09e07547d747129947506c01423
Paul Stoellberger <p.stoellberger@gmail.com>
2011-05-15 23:28:29 +0100

    make changes in build process according to new ui

--- buildall.bat
+++ buildall.bat
@@ -3,6 +3,8 @@ call mvn clean install
 cd ..
 cd saiku-webapp
 call mvn clean scm:checkout install
-cd ..
-cd saiku-server
+cd ../saiku-ui
+cd saiku-ui
+call mvn clean package install:install-file -Dfile=target/saiku-ui-2.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0-SNAPSHOT -Dpackaging=war
+cd ../saiku-server
 call mvn clean package
