2ac339636ecb2f488bca841b40b4ce7c64035c79
Paul Stoellberger <p.stoellberger@gmail.com>
2011-05-15 23:40:10 +0100

    more changes to build workflow

--- buildall.bat
+++ buildall.bat
@@ -2,7 +2,7 @@ cd saiku-core
 call mvn clean install
 cd ..
 cd saiku-webapp
-call mvn clean scm:checkout install
+call mvn clean 
 cd ../saiku-ui
 cd saiku-ui
 call mvn clean package install:install-file -Dfile=target/saiku-ui-2.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0-SNAPSHOT -Dpackaging=war
