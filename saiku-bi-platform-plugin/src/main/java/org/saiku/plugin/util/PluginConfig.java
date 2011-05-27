4d43578846e8e73e1659f348ba5334102d856379
Hudson <mscahill@stetson.edu>
2011-05-27 15:39:41 -0400

    Wrong folder

--- saiku-bi-platform-plugin/src/main/java/org/saiku/plugin/util/PluginConfig.java
+++
@@ -1,9 +0,0 @@
-package org.saiku.plugin.util;
-
-public class PluginConfig {
-    
-    public static final String PLUGIN_NAME = "saiku analytics";
-    public static final String ICON = "saiku_16x16.png";
-    
-
-}
