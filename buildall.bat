14e0061654fe10a5fce48607a1d7110fd2dd2f2b
Paul Stoellberger <p.stoellberger@gmail.com>
2011-05-29 20:36:48 +0200

    switch to 2.0

--- buildall.bat
+++ buildall.bat
@@ -5,6 +5,6 @@ cd saiku-webapp
 call mvn clean 
 cd ../saiku-ui
 cd saiku-ui
-call mvn clean package install:install-file -Dfile=target/saiku-ui-2.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0-SNAPSHOT -Dpackaging=war
+call mvn clean package install:install-file -Dfile=target/saiku-ui-2.0.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0 -Dpackaging=war
 cd ../saiku-server
 call mvn clean package
--- buildall.sh
+++ buildall.sh
@@ -6,7 +6,7 @@ cd saiku-webapp
 mvn clean 
 cd ..
 cd saiku-ui
-mvn clean package install:install-file -Dfile=target/saiku-ui-2.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0-SNAPSHOT -Dpackaging=war
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.0.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.0 -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 
