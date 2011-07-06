b2a000354f54c4ddaf73f413c61e95eaf1eb1c92
Mark Cahill <mark@tiemonster.info>
2011-07-06 16:12:52 -0400

    Implemented asynchronous template loader

--- js/saiku/views/Workspace.js
+++ js/saiku/views/Workspace.js
@@ -1,3 +1,33 @@
-var Workspace = Backbone.View.extend({
+var WorkspaceToolbar = Backbone.View.extend({
+    template: function() {
+        return Saiku.template.get('WorkspaceToolbar')();
+    },
     
+    render: function() {
+        $(this.el).html(this.template());
+        
+        return this; 
+    }
+});
+
+var Workspace = Backbone.View.extend({
+    template: function() {
+        return Saiku.template.get('Workspace')();        
+    },
+    
+    render: function() {
+        // Load template
+        $(this.el).html(this.template());
+        
+        // Generate toolbar
+        this.toolbar.render();
+        $(this.el).find('.workspace_toolbar').append($(this.toolbar.el));
+        
+        return this; 
+    },
+    
+    initialize: function() {
+        // Generate toolbar and append to workspace
+        this.toolbar = new WorkspaceToolbar;
+    }
 });
