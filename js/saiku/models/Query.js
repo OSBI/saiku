0afeabb56851a84704bfe0ae31d35d914c4f3a43
Mark Cahill <mark@tiemonster.info>
2011-07-07 16:32:46 -0400

    I want to NOT set the id, so it will POST it instead of PUT it

--- js/saiku/models/Query.js
+++ js/saiku/models/Query.js
@@ -2,7 +2,8 @@ var Query = Backbone.Model.extend({
     initialize: function(args) {
         this.cube = args.cube;
         
-        this.id = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        this.name = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'
+        .replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0,
             v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
