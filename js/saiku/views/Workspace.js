42d0b904cd2476ab8f39e5f89bbff1642889bd7e
Mark Cahill <mark@tiemonster.info>
2011-07-08 08:37:15 -0400

    Split out the workpsace toolbar and removed some debugging

--- js/saiku/views/Workspace.js
+++ js/saiku/views/Workspace.js
@@ -1,39 +1,3 @@
-var WorkspaceToolbar = Backbone.View.extend({
-    enabled: false,
-    events: {
-        'click a': 'call'
-    },
-    
-    template: function() {
-        return Saiku.template.get('WorkspaceToolbar')();
-    },
-    
-    render: function() {
-        $(this.el).html(this.template());
-        
-        return this; 
-    },
-    
-    call: function(args) {
-        // Determine callback
-        var callback = args.target.hash.replace('#', '');
-        
-        // Attempt to call callback
-        try {
-            this.enabled && this[callback](args);
-        } catch (e) {
-            console && 
-                console.log('Workspace callback ' + callback + " does not exist");
-        }
-        
-        return false;
-    },
-    
-    save_query: function() {
-        console.log('save');
-    }
-});
-
 var Workspace = Backbone.View.extend({
     className: 'tab_container',
     
