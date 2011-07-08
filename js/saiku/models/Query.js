4e32744fe53d0aa1b4945fa6e5640585937ea8e9
Mark Cahill <mark@tiemonster.info>
2011-07-08 08:23:08 -0400

    Create new query on workspace

--- js/saiku/models/Query.js
+++ js/saiku/models/Query.js
@@ -2,12 +2,28 @@ var Query = Backbone.Model.extend({
     initialize: function(args) {
         this.cube = args.cube;
         
+        // Generate a unique query id
         this.name = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'
         .replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0,
             v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         }).toUpperCase();
+        
+        // Parse cube information
+        this.parse_cube();
+        
+        // Create the query on the server workspace
+        console.log('saving');
+        this.save();
+    },
+    
+    parse_cube: function() {
+        var parsed_cube = this.cube.split('/');
+        this.attributes.connection = parsed_cube[0];
+        this.attributes.catalog = parsed_cube[1];
+        this.attributes.schema = parsed_cube[2];
+        this.attributes.cube = parsed_cube[3];
     },
     
     url: function() {
