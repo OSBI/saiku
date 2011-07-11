dc02f4da43807b7f3f349625921ce0b2ca471f8a
Mark Cahill <mark@tiemonster.info>
2011-07-11 15:56:50 -0400

    Added draggable behavior

--- js/saiku/views/DimensionList.js
+++ js/saiku/views/DimensionList.js
@@ -9,8 +9,13 @@ var DimensionList = Backbone.View.extend({
     },
     
     initialize: function(args) {
+        // Don't lose this
         _.bindAll(this, "render");
         
+        // Bind parent element
+        this.workspace = args.workspace;
+        
+        // Fetch from the server if we haven't already
         if (! args.dimension.has('template')) {
             args.dimension.fetch({ 
                 success: function() {
@@ -22,12 +27,26 @@ var DimensionList = Backbone.View.extend({
             this.template = args.dimension.get('template');
             this.render();
         }
-        
     },
     
     render: function() {
+        // Pull the HTML from cache and hide all dimensions
         $(this.el).html(this.template)
             .find('.hide').hide().removeClass('hide');
+        
+        // Add draggable behavior
+        $(this.el).find('.measure,.dimension').draggable({
+            cancel: '.not-draggable, .hierarchy',
+            connectToSortable: $(this.workspace.el)
+                .find('.columns > ul, .rows > ul, .filter > ul'),
+            helper: 'clone',
+            opacity: 0.60,
+            tolerance: 'pointer',
+            cursorAt: {
+                top: 10,
+                left: 35
+            }
+        });
     },
     
     select: function(event) {
