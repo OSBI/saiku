60426703e077e3016f8331a8b4598fd45109cec6
Mark Cahill <mark@tiemonster.info>
2011-07-12 15:55:05 -0400

    Automatic query execution

--- js/saiku/views/WorkspaceToolbar.js
+++ js/saiku/views/WorkspaceToolbar.js
@@ -5,6 +5,7 @@ var WorkspaceToolbar = Backbone.View.extend({
     },
     
     initialize: function(args) {
+        // Keep track of parent workspace
         this.workspace = args.workspace;
     },
     
@@ -24,7 +25,7 @@ var WorkspaceToolbar = Backbone.View.extend({
         
         // Attempt to call callback
         try {
-            ! $(event.target).hasClass('disabled_toolbar') && this[callback](args);
+            ! $(event.target).hasClass('disabled_toolbar') && this[callback](event);
         } catch (e) {
             console && 
                 console.log("Could not fire " + callback + ": " + e.message);
@@ -33,7 +34,20 @@ var WorkspaceToolbar = Backbone.View.extend({
         return false;
     },
     
-    save_query: function() {
+    save_query: function(event) {
         // TODO - save query
+    },
+    
+    automatic_execution: function(event) {
+        // Change property
+        var new_value = ! this.workspace.query.properties
+            .get('saiku.olap.query.automatic_execution');
+        this.workspace.query.properties.set({
+            'saiku.olap.query.automatic_execution': new_value
+        });
+        console.log(this.workspace.query.properties);
+        
+        // Toggle state of button
+        $(event.target).toggleClass('on');
     }
 });
