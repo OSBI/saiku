f8afb5d1f006fabb9b448d3cd59e78a3032c2d3b
Mark Cahill <mark@tiemonster.info>
2011-07-18 16:31:43 -0400

    Initialize chart plugin

--- js/saiku/models/Plugin.js
+++ js/saiku/models/Plugin.js
@@ -3,8 +3,8 @@
  */
 var Plugin = Backbone.Model.extend({
     initialize: function() {        
-        for (var i = 0; i < Settings.plugins.length; i++) {
-            $.getScript("/js/saiku/plugins/" + Settings.plugins[i] + ".js");
+        for (var i = 0; i < Settings.PLUGINS.length; i++) {
+            $.getScript("/js/saiku/plugins/" + Settings.PLUGINS[i] + ".js");
         }
     }
 });
