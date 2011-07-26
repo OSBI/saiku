0ad80917976312f4fbd62f032cf97b1fd71c0920
Mark Cahill <mark@tiemonster.info>
2011-07-26 15:33:12 +0000

    Working on a version of charts that utilizes CCC

--- js/saiku/plugins/Chart/plugin.js
+++ js/saiku/plugins/Chart/plugin.js
@@ -16,12 +16,12 @@ var Chart = Backbone.View.extend({
         if (! this.workspace.query) {
             this.workspace.bind('query:new', this.activate_button);
         } else {
-            this.activate_button();
+            this.activate_button({ workspace: this });
         }
         
         // Append chart to workspace
         $(this.workspace.el).find('.workspace_results')
-            .prepend($(this.el).hide());
+            .prepend($(this.el));
     },
     
     add_button: function() {
@@ -39,31 +39,35 @@ var Chart = Backbone.View.extend({
         $(this.workspace.el).find('.workspace_results table').toggle();
         $(this.el).toggle();
         $(event.target).toggleClass('on');
-        
-        if ($(event.target).hasClass('on')) {
-            _.delay(this.render, 10);
-        }
     },
     
-    render: function() {        
+    render: function() { 
         $(this.el).css({
             width: $(this.workspace.el).find('.workspace_results').width() - 50,
             height: $(this.workspace.el).find('.workspace_results').height()
-        }).html('').show();
+        }).html('');
         
-        this._chart = new Saiku.YUI.Chart({
-            dataProvider: this.data, 
-            render: this.el,
-            categoryAxis: this.id,
-            type: this.type ? this.type : "column",
-            styles: {
-                axes: {
-                    category: {
-                        rotation: 90
-                    }
-				}
-			}
-        });
+        try {
+            this.chart = new pvc.BarChart({
+                canvas: this.id,
+                width: $(this.workspace.el).find('.workspace_results').width() - 50,
+                height: $(this.workspace.el).find('.workspace_results').height(),
+                orientation: 'horizontal',
+                stacked: true,
+                animate: false,
+                legend: true,
+                legendPosition:"bottom"
+            });
+            
+            this.chart.setData(this.data, {
+                crosstabMode: true,
+                seriesInRows: false
+            });
+            
+            this.chart.render();
+        } catch (e) {
+            console.log(e);
+        }
     },
     
     receive_data: function(args) {
@@ -71,39 +75,41 @@ var Chart = Backbone.View.extend({
     },
     
     process_data: function(args) {
-        this.data = [];       
+        this.data = {};
+        this.data.resultset = [];
+        this.data.metadata = [];
+        
+        for (var field = 1; field < args.data[0].length; field++) {
+            this.data.metadata[field - 1] = {
+                colIndex: field - 1,
+                colType: String,
+                colName: args.data[row][field].value
+            };
+        }
         
         if (args.data.length > 0) {
             for (var row = 1; row < args.data.length; row++) {
-                var record = {};
+                var record = [];
                 record.category = args.data[row][0].value;
                 for (var col = 1; col < args.data[row].length; col++) {
-                    record[args.data[0][col].value] = 
-                        parseFloat(args.data[row][col].value
-                            .replace(/[^a-zA-Z 0-9.]+/g,''));
+                    record.push(parseFloat(args.data[row][col].value
+                        .replace(/[^a-zA-Z 0-9.]+/g,'')));
                 }
                 this.data.push(record);
             }
             
-            if ($(this.workspace.el).find('.chart').hasClass('on')) {
-                this.render();
-            }
+            console.log(this.data);
+            
+            this.render();
         }
     }
 });
 
 (function() {
     // Initialize YUI
-    YUI().use('charts', function (Y) {
-        Saiku.YUI = Y;
-        Saiku.YUI.Histogram.prototype._defaultFillColors = [
-            "#A90D0B", "#333333", "#666666"
-        ];
-        
+    $.getScript("js/saiku/plugins/Chart/ccc.js", function() {
         function new_workspace(args) {
             // Add chart element
-            console.log("Adding chart to new workspace");
-            console.log(args.workspace.el);
             args.workspace.chart = new Chart({ workspace: args.workspace });
         }
         
