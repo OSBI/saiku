5d88f2db865963280f1f428ccc3c0049e7e8fc42
pstoellberger <pstoellberger@gmail.com>
2011-08-15 23:27:37 +0100

    switch to 2.1

--- buildall.bat
+++ buildall.bat
@@ -5,7 +5,7 @@ cd saiku-webapp
 call mvn clean 
 cd ../saiku-ui
 cd saiku-ui
-call mvn clean package install:install-file -Dfile=target/saiku-ui-2.1-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.1-SNAPSHOT -Dpackaging=war
+call mvn clean package install:install-file -Dfile=target/saiku-ui-2.1.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.1 -Dpackaging=war
 cd ../saiku-server
 call mvn clean package
 cd ../saiku-bi-platform-plugin
