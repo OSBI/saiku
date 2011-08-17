f2f788d4decd4aaf45fc68f93ba58aa77880ca41
Mark Cahill <mark@tiemonster.info>
2011-08-17 11:44:22 -0400

    Extend settings with query parameters. Fixes #60

--- js/saiku/Settings.js
+++ js/saiku/Settings.js
@@ -3,7 +3,7 @@
  */
 var Settings = {
     VERSION: "Saiku Suite 2.2 SNAPSHOT",
-    PLUGIN: false,
+    BIPLUGIN: false,
     BASE_URL: "",
     TOMCAT_WEBAPP: "/saiku",
     REST_MOUNT_POINT: "/rest/saiku/",
@@ -20,6 +20,29 @@ var Settings = {
     ],
     LOCALSTORAGE_EXPIRATION: 10 * 60 * 60 * 1000 /* 10 hours, in ms */
 };
+
+/**
+ * Extend settings with query parameters
+ */
+Settings.GET = function () {
+    var qs = document.location.search;
+    qs = qs.split("+").join(" ");
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        var value = decodeURIComponent(tokens[2]);
+        if (! isNaN(value)) value = parseInt(value);
+        if (value === "true") value = true;
+        if (value === "false") value = false;
+        params[decodeURIComponent(tokens[1]).toUpperCase()]
+            = value;
+    }
+
+    return params;
+}();
+_.extend(Settings, Settings.GET);
 
 Settings.REST_URL = Settings.BASE_URL
     + Settings.TOMCAT_WEBAPP 
