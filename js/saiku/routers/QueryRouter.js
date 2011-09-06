c72d534e7f9d14d49972170667128d15c2405509
Mark Cahill <mark@tiemonster.info>
2011-09-06 15:13:20 -0400

    Send parameters as lowercase. Can't test because demo server is unavailable.

--- js/saiku/routers/QueryRouter.js
+++ js/saiku/routers/QueryRouter.js
@@ -8,7 +8,14 @@ var QueryRouter = Backbone.Router.extend({
     
     open_query: function(query_name) {
         Settings.ACTION = "OPEN_QUERY";
-        var query = new SavedQuery(_.extend({ name: query_name }, Settings.GET));
+        var options = { 
+            name: query_name,
+            solution: Settings.GET.SOLUTION || "",
+            path: Settings.GET.PATH || "",
+            action: Settings.GET.ACTION || "",
+            biplugin: true
+        };
+        var query = new SavedQuery(options);
         query.fetch({ success: query.move_query_to_workspace });
     }
 });
