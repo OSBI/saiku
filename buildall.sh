6260a1e369de23cc0e77e06b561593e31173cb2a
pstoellberger <pstoellberger@gmail.com>
2011-10-05 23:19:53 +0200

    add saiku script and modify buildall.sh, update ui submodule

--- buildall.sh
+++ buildall.sh
@@ -6,8 +6,7 @@ cd saiku-webapp
 mvn clean install
 cd ..
 cd saiku-ui
-# mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin
-mvn clean package
