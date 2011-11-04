578cc9e294d7954b958f6a62e68fea3c805704a8
Tom Barber <tom@analytical-labs.com>
2011-11-04 10:56:08 +0000

    add gpl licences

--- saiku-core/saiku-service/src/main/java/org/saiku/olap/util/SaikuObjectComparator.java
+++ saiku-core/saiku-service/src/main/java/org/saiku/olap/util/SaikuObjectComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Paul Stoellberger
+ * Copyright (C) 2011 OSBI Ltd
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
@@ -17,7 +17,6 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  *
  */
-
 package org.saiku.olap.util;
 
 import java.util.Comparator;
--- saiku-core/saiku-service/src/main/java/org/saiku/service/olap/OlapQueryService.java
+++ saiku-core/saiku-service/src/main/java/org/saiku/service/olap/OlapQueryService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Paul Stoellberger
+ * Copyright (C) 2011 OSBI Ltd
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
