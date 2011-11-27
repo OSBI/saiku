d9562b4c8f3cf2a7994b9108c7fe84d0268e5ff2
pstoellberger <pstoellberger@gmail.com>
2011-11-27 14:55:43 +0000

    fix #90 - add ui building to buildall.sh /.bat

--- buildall.bat
+++ buildall.bat
@@ -2,10 +2,13 @@ cd saiku-core
 call mvn clean install
 cd ..
 cd saiku-webapp
-call mvn clean 
-cd ../saiku-ui
+call mvn clean install
+cd ..
+call git submodule init
+call git submodule update
 cd saiku-ui
-# call mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
+call git fetch
+call mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 call mvn clean package
 cd ../saiku-bi-platform-plugin
--- buildall.sh
+++ buildall.sh
@@ -6,8 +6,11 @@ cd ..
 cd saiku-webapp
 mvn clean install
 cd ..
+git submodule init
+git submodule update
 cd saiku-ui
-#mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
+git fetch
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.2-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.2-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin
