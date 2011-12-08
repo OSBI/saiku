656a9f79848e02a7f9f9309c8ff48cd880f683b8
pstoellberger <pstoellberger@gmail.com>
2011-12-08 01:20:46 +0100

    implement Array.indexOf() for < IE9 (fixes IE8 issues)

--- js/saiku/Settings.js
+++ js/saiku/Settings.js
@@ -68,3 +68,30 @@ _.extend(Settings, Settings.GET);
 Settings.REST_URL = Settings.BASE_URL
     + Settings.TOMCAT_WEBAPP 
     + Settings.REST_MOUNT_POINT;
+
+
+/**
+ * < IE9 doesn't support Array.indexOf
+ */
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
