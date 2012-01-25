0306d3f6b071362a8dca402036b44cfdb1149c48
pstoellberger <pstoellberger@gmail.com>
2012-01-25 12:57:49 +0100

    allow encoded slash for the rest urls

--- saiku-server/scripts/start-saiku.bat
+++ saiku-server/scripts/start-saiku.bat
@@ -6,7 +6,7 @@ call "%~dp0\set-java.bat"
 
 cd tomcat\bin
 set CATALINA_HOME=%~dp0tomcat
-set CATALINA_OPTS=-Xms512m -Xmx768m -XX:MaxPermSize=256m -Dfile.encoding=UTF-8
+set CATALINA_OPTS=-Xms512m -Xmx768m -XX:MaxPermSize=256m -Dfile.encoding=UTF-8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true
 set JAVA_HOME=%_JAVA_HOME%
 call startup
 :quit
