41ee241046967c936e1917b8a6f12bf222d58955
pstoellberger <pstoellberger@gmail.com>
2012-03-01 12:28:23 +0100

    change plugin title

--- saiku-bi-platform-plugin/src/main/java/org/saiku/plugin/util/PluginConfig.java
+++ saiku-bi-platform-plugin/src/main/java/org/saiku/plugin/util/PluginConfig.java
@@ -21,7 +21,7 @@ package org.saiku.plugin.util;
 
 public class PluginConfig {
     
-    public static final String PLUGIN_NAME = "saiku analytics";
+    public static final String PLUGIN_NAME = "saiku";
     public static final String ICON = "saiku_16x16.png";
     
 
