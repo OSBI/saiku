203ebe35ce12f4fc80f1ce813557f76dcef48edd
pstoellberger <pstoellberger@gmail.com>
2012-04-03 18:06:57 +0200

    upgrade to 2.4-SNAPSHOT

--- buildall.bat
+++ buildall.bat
@@ -9,7 +9,7 @@ call git submodule update
 cd saiku-ui
 call git checkout master
 call git pull
-call mvn clean package install:install-file -Dfile=target/saiku-ui-2.3-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.3-SNAPSHOT -Dpackaging=war
+call mvn clean package install:install-file -Dfile=target/saiku-ui-2.4-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 call mvn clean package
 cd ../saiku-bi-platform-plugin
--- release.sh
+++ release.sh
@@ -9,9 +9,9 @@ cd ..
 git submodule init
 git submodule update
 cd saiku-ui
-git checkout master 
-git pull
-mvn clean package install:install-file -Dfile=target/saiku-ui-2.4-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4-SNAPSHOT -Dpackaging=war
+git checkout release 
+git pull origin release
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.3.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.3 -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin
