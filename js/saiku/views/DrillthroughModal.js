6c028e0fce0b4d4f353576bb9eb3942f2a695c31
pstoellberger <pstoellberger@gmail.com>
2012-04-28 00:28:31 +0200

    some cleanup for drill through modal

--- js/saiku/views/DrillthroughModal.js
+++ js/saiku/views/DrillthroughModal.js
@@ -57,20 +57,15 @@ var DrillthroughModal = Modal.extend({
                             localStorage.getItem("measure." + key) !== null) {
                             Saiku.session.sessionworkspace.dimensions[key] = new Dimension(JSON.parse(localStorage.getItem("dimension." + key)));
                             Saiku.session.sessionworkspace.measures[key] = new Measure(JSON.parse(localStorage.getItem("measure." + key)));
-                            console.log("fetch from localstorage");
                         } else {
                             Saiku.session.sessionworkspace.dimensions[key] = new Dimension({ key: key });
                             Saiku.session.sessionworkspace.measures[key] = new Measure({ key: key });
                             Saiku.session.sessionworkspace.dimensions[key].fetch({ async : false });
                             Saiku.session.sessionworkspace.measures[key].fetch({ async : false });
-                            console.log("fetch from server");
                         }
                         dimensions = Saiku.session.sessionworkspace.dimensions[key].get('data');
                         measures = Saiku.session.sessionworkspace.measures[key].get('data');
-        } else {
-            console.log("fetch got it");
-        }
-
+        } 
 
         var templ_dim =_.template($("#template-drillthrough-dimensions").html())({dimensions: dimensions});
         var templ_measure =_.template($("#template-drillthrough-measures").html())({measures: measures});
