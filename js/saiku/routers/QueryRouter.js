8cd36543067a83c7869436721b8613dfcbc2d241
pstoellberger <pstoellberger@gmail.com>
2012-07-06 16:03:18 +0200

    fix query router

--- js/saiku/routers/QueryRouter.js
+++ js/saiku/routers/QueryRouter.js
@@ -29,15 +29,25 @@ var QueryRouter = Backbone.Router.extend({
     
     open_query: function(query_name) {
         Settings.ACTION = "OPEN_QUERY";
-        var options = { 
-            name: query_name,
-            solution: Settings.GET.SOLUTION || "",
-            path: Settings.GET.PATH || "",
-            action: Settings.GET.ACTION || "",
-            biplugin: true
-        };
+        var options = {};
+        var dataType = "json"
+        if (Settings.BIPLUGIN) {
+            options = {
+                name: query_name,
+                solution: Settings.GET.SOLUTION || "",
+                path: Settings.GET.PATH || "",
+                action: Settings.GET.ACTION || "",
+                biplugin: true
+            };
+        } else {
+            dataType = "text"
+            options = {
+                file: query_name,
+                dataType: "text"
+            }
+        }
         var query = new SavedQuery(options);
-        query.fetch({ success: query.move_query_to_workspace });
+        query.fetch({ success: query.move_query_to_workspace, dataType: dataType});
     },
 
     open_query_repository: function( ) {
