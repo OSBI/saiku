9dd45890d4a043a6a3bb97dbb0a7ef22aded5609
pstoellberger <pstoellberger@gmail.com>
2012-07-11 14:18:22 +0200

    fix image path

--- js/saiku/views/Table.js
+++ js/saiku/views/Table.js
@@ -366,7 +366,7 @@ var Table = Backbone.View.extend({
                         val = "<a target='__blank' href='" + header.properties.link + "'>" + val + "</a>";
                     }
                     if (header.properties.hasOwnProperty('arrow')) {
-                        arrow = "<img height='10' width='10' style=\"padding-left: 5px\" src=\"/saiku-ui/images/arrow-" + header.properties.arrow + ".gif\" border=\"0\">";
+                        arrow = "<img height='10' width='10' style='padding-left: 5px' src='/images/arrow-" + header.properties.arrow + ".gif' border='0'>";
                     }
 
                     contents += '<td class="data" ' + color + '><div alt="' + header.properties.raw + '" rel="' + header.properties.position + '">' + val + arrow + '</div></td>';
