1315efcaaaaa5f0a1adfd0e327a9600f57b06665
pstoellberger <pstoellberger@gmail.com>
2012-07-19 18:55:58 +0200

    adapt buildalls cripts for new project

--- buildall.sh
+++ buildall.sh
@@ -14,5 +14,7 @@ git pull
 mvn clean package install:install-file -Dfile=target/saiku-ui-2.4-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
+cd ../saiku-bi-platform-plugin-util
+mvn clean install
 cd ../saiku-bi-platform-plugin
 mvn clean package
