01a5323e8017a0471d76c7ba81a440c19b23f262
Marco Ferretti <marco.ferretti@gmail.com>
2012-07-31 22:31:57 +0200

    Added javadoc

--- saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/acl2/Acl.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/rest/objects/acl2/Acl.java
@@ -26,8 +26,17 @@ import org.saiku.web.rest.objects.acl2.enumeration.AclMethod;
  */
 public class Acl {
 
+	/**
+	 * holds the actual acl map
+	 */
 	private Map<String, AclEntry> acl = new TreeMap<String, AclEntry>();
-
+	/**
+	 * Returns the access method to the specified resource for the user or role
+	 * @param resource the resource to which you want to access
+	 * @param username the username of the user that's accessing
+	 * @param role the role of the user that's accessing
+	 * @return {@link AclMethod}
+	 */
 	public AclMethod getMethod(FileObject resource, String username, String role) {
 		String name = resource.getName().getPath();
 		AclEntry entry = acl.get(name);
@@ -62,7 +71,7 @@ public class Acl {
 						// no role nor user acl
 						method = AclMethod.NONE;
 					} else {
-						// return the stongest role
+						// return the strongest role
 						method = AclMethod.max(roleMethods);
 					}
 				} else {
@@ -83,35 +92,56 @@ public class Acl {
 		return method;
 	}
 
+	/**
+	 * helper method to add an acl entry
+	 * @param resource resource for which we're setting the 
+	 * access control
+	 * @param entry
+	 */
 	public void addEntry(FileObject resource, AclEntry entry) {
 		acl.put(resource.getName().getPath(), entry);
 	}
-
+	/**
+	 * Helper method to test if the resource is readable by the 
+	 * user or role
+	 * @param resource the resource being tested
+	 * @param username the user name that wants to access 
+	 * @param role the role of the user that wants access
+	 * @return true if the resource is marked > {@link AclMethod#NONE} 
+	 */
 	public boolean canRead(FileObject resource, String username, String role) {
+		// if > NONE then can read
 		return !AclMethod.max(AclMethod.NONE,
-				getMethod(resource, username, role)).equals(AclMethod.NONE); // if
-																				// >
-																				// NONE
-																				// then
-																				// can
-																				// read
+				getMethod(resource, username, role)).equals(AclMethod.NONE); 
 	}
-
+	/**
+	 * Helper method to test if the resource is writeable by a user or role
+	 * @param resource the resource being tested
+	 * @param username the user name that wants to access 
+	 * @param role the role of the user that wants access
+	 * @return true if the resource is marked  {@link AclMethod#WRITE} or {@link AclMethod#GRANT} 
+	 */
 	public boolean canWrite(FileObject resource, String username, String role) {
 		AclMethod method = getMethod(resource, username, role);
 		return method.equals(AclMethod.WRITE) || method.equals(AclMethod.GRANT);
 	}
-
+	/**
+	 * Helper method to test if the resource is grantable by a user or role
+	 * @param resource the resource being tested
+	 * @param username the user name that wants to access 
+	 * @param role the role of the user that wants access
+	 * @return true if the resource is marked  {@link AclMethod#GRANT} 
+	 */
 	public boolean canGrant(FileObject resource, String username, String role) {
 		return !AclMethod.max(AclMethod.GRANT,
-				getMethod(resource, username, role)).equals(AclMethod.GRANT); // if
-																				// >
-																				// NONE
-																				// then
-																				// can
-																				// read
+				getMethod(resource, username, role)).equals(AclMethod.GRANT); 
 	}
-
+	/**
+	 * Searches the acl file for the resource in the resource's directory
+	 * ( or inside teh resource's childre if the resource is a directory ), 
+	 * reads the file and adds all the acl entries to the global acl map
+	 * @param resource
+	 */
 	public void readAcl(FileObject resource) {
 		FileObject jsonFile = null;
 		try {
@@ -123,41 +153,56 @@ public class Acl {
 		} catch (Exception e) {
 			// TODO some decent logging
 		}
-		acl.putAll(deserialize(resource));
+		acl.putAll(deserialize(jsonFile));
 	}
 
+	/**
+	 * writes the acl of the resource in the resource's directory
+	 * @param resource
+	 */
 	public void writeAcl(FileObject resource) {
 		//TODO : method 
 	}
-
-	private void serialize(FileObject jsonFile) {
+	/**
+	 * Serializes in json format the acl passed as parameter into 
+	 * the specified file
+	 * @param jsonFile the file where to write the acl
+	 * @param acl the acl to serialize
+	 */
+	private void serialize(FileObject jsonFile, Map<String,AclEntry> acl) {
 		//TODO : method
 	}
-
+	/**
+	 * internal method. reads the acl file and creates an acl map
+	 * @param jsonFile the file containing the acl 
+	 * @return the acl map represented by the acl file
+	 */
 	private Map<String, AclEntry> deserialize(FileObject jsonFile) {
 		ObjectMapper mapper = new ObjectMapper();
 		FileContent fc;
 		InputStream is;
 		Map<String, AclEntry> acl = new TreeMap<String, AclEntry>();
-		try {
-			fc = jsonFile.getContent();
-			is = fc.getInputStream();
-			acl = (Map<String, AclEntry>) mapper.readValue(is, TypeFactory
-					.mapType(HashMap.class, String.class, AclEntry.class));
-			fc.close();
-			is.close();
-		} catch (FileSystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if ( jsonFile != null ) {
+			try {
+				fc = jsonFile.getContent();
+				is = fc.getInputStream();
+				acl = (Map<String, AclEntry>) mapper.readValue(is, TypeFactory
+						.mapType(HashMap.class, String.class, AclEntry.class));
+				fc.close();
+				is.close();
+			} catch (FileSystemException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return acl;
 	}
