152a6eef020407446d384194ba7b759c9bbb16ef
pstoellberger <pstoellberger@gmail.com>
2012-10-09 19:11:32 +0200

    bump version to 2.4

--- buildall.sh
+++ buildall.sh
@@ -11,7 +11,7 @@ git submodule update
 cd saiku-ui
 git checkout master 
 git pull
-mvn clean package install:install-file -Dfile=target/saiku-ui-2.4-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4-SNAPSHOT -Dpackaging=war
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.4.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.4 -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin-util
