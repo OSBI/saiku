493a3d48eae6d9fb8e7f2a0eb1c422a675588f31
pstoellberger <pstoellberger@gmail.com>
2013-01-18 16:57:48 +0100

    switch to qm ui

--- buildall.sh
+++ buildall.sh
@@ -8,7 +8,7 @@ cd ..
 git submodule init
 git submodule update
 cd saiku-ui
-git checkout master 
+git checkout qm 
 git pull
 mvn clean package install:install-file -Dfile=target/saiku-ui-3.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=3.0-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
