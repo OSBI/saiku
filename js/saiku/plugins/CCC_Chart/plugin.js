2d5206f95bb34ad3ade635d50d2830dad759d490
pstoellberger <pstoellberger@gmail.com>
2013-05-24 00:31:18 +0200

    change default color scheme

--- js/saiku/plugins/CCC_Chart/plugin.js
+++ js/saiku/plugins/CCC_Chart/plugin.js
@@ -315,7 +315,7 @@ var Chart = Backbone.View.extend({
             axisSizeMax: "40%",
             plotFrameVisible : false,
             orthoAxisMinorTicks : false,
-            colors: ["#4bb2c5", "#c5b47f", "#EAA228", "#579575", "#839557", "#958c12", "#953579", "#4b5de4", "#d8b83f", "#ff5800", "#0085cc"]
+            colors: ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5" ]
         },
         
         HeatGridChart: {
