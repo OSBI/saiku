509b49fba7e23ef89384e54aeb75ac5e6295acf6
pstoellberger <pstoellberger@gmail.com>
2013-11-14 16:52:59 +0100

    fix submodule issue

--- buildall.sh
+++ buildall.sh
@@ -8,8 +8,9 @@ cd ..
 git submodule init
 git submodule update
 cd saiku-ui
+git pull
 git checkout saiku3 
-git pull origin qm
+git pull origin saiku3
 mvn clean package install:install-file -Dfile=target/saiku-ui-3.0-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=3.0-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
