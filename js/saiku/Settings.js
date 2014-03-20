78ec2fdaded5e84151612bac16cbb2b86430f467
pstoellberger <pstoellberger@gmail.com>
2014-03-20 18:11:25 +0100

    disable upgrade setting for demo

--- js/saiku/Settings.js
+++ js/saiku/Settings.js
@@ -175,6 +175,7 @@ if (window.location.hostname && (window.location.hostname == "dev.analytical-lab
     Settings.USERNAME = "admin";
     Settings.PASSWORD = "admin";
     Settings.DEMO = true;
+    Settings.UPGRADE = false;
 }
 
 var isIE = (function(){
