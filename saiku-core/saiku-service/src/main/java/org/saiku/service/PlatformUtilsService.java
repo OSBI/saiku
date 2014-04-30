a2cd786a5e0f96202a4dcbec35ba0290b719d780
Tom Barber <tom@analytical-labs.com>
2014-04-30 21:55:36 +0100

    plugin detection

---
+++ saiku-core/saiku-service/src/main/java/org/saiku/service/PlatformUtilsService.java
@@ -0,0 +1,57 @@
+package org.saiku.service;
+
+import org.saiku.service.util.Plugin;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+
+/**
+ * Created by bugg on 30/04/14.
+ */
+public class PlatformUtilsService {
+
+  private String filePath;
+
+  @Autowired
+  public void setPath(String path) {
+    this.filePath = path;
+  }
+
+  public String getPath(){
+    return filePath;
+  }
+
+
+  public ArrayList getAvailablePlugins(){
+    ArrayList l = new ArrayList<Plugin>(  );
+    File f = new File(filePath);
+
+    String[] directories = f.list(new FilenameFilter() {
+      public boolean accept(File current, String name) {
+        return new File(current, name).isDirectory();
+      }
+    });
+
+    if(directories != null && directories.length>0) {
+      for ( String d : directories ) {
+        File subdir = new File( filePath+"/"+d );
+        File[] subfiles = subdir.listFiles();
+
+        /**
+         * TODO use a metadata.js file for alternative details.
+         */
+        if ( subfiles != null ) {
+          for ( File s : subfiles ) {
+            if ( s.getName().equals( "plugin.js" ) ) {
+              Plugin p = new Plugin( s.getParentFile().getName(), "", "js/saiku/plugins/" + s.getParentFile().getName() + "/plugin.js" );
+              l.add( p );
+            }
+          }
+        }
+      }
+    }
+    return l;
+  }
+}
---
+++ saiku-core/saiku-service/src/main/java/org/saiku/service/util/Plugin.java
@@ -0,0 +1,41 @@
+package org.saiku.service.util;
+
+/**
+ * Created by bugg on 30/04/14.
+ */
+public class Plugin {
+
+  private String name;
+  private String description;
+  private String path;
+
+  public Plugin( String name, String description, String path ) {
+    this.name = name;
+    this.description = description;
+    this.path = path;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName( String name ) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription( String description ) {
+    this.description = description;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath( String path ) {
+    this.path = path;
+  }
+}
