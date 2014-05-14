e6e780e66cd78d9136eb8c07c1224818bb05bf2e
Tom Barber <tom@analytical-labs.com>
2014-05-14 10:21:10 +0100

    tidy up tests

--- saiku-core/saiku-service/src/test/java/org/saiku/DataSteps.java
+++ saiku-core/saiku-service/src/test/java/org/saiku/DataSteps.java
@@ -1,171 +1,164 @@
+/*
+ *   Copyright 2012 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package org.saiku;
 
 import net.thucydides.core.annotations.Step;
-import net.thucydides.core.annotations.StepProvider;
 import org.junit.Test;
 import org.saiku.datasources.connection.IConnectionManager;
+import org.saiku.datasources.connection.impl.SimpleConnectionManager;
 import org.saiku.datasources.datasource.SaikuDatasource;
 import org.saiku.olap.discover.OlapMetaExplorer;
 import org.saiku.olap.dto.SaikuConnection;
 import org.saiku.olap.util.exception.SaikuOlapException;
+import org.saiku.service.DatabaseHelper;
 import org.saiku.service.datasource.ClassPathResourceDatasourceManager;
+import org.saiku.service.datasource.DatasourceService;
 import org.saiku.service.datasource.IDatasourceManager;
+import org.saiku.service.olap.OlapDiscoverService;
+import org.saiku.service.olap.ThinQueryService;
 import org.saiku.service.util.exception.SaikuServiceException;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 /**
- * Created by bugg on 02/05/14.
+ * Data steps for OlapDataSourceStepsdef
  */
 public class DataSteps {
 
-  private static OlapMetaExplorer olapMetaExplorer;
+  public static boolean DEBUG = false;
   private static Properties testProps = new Properties();
+  public IDatasourceManager datasourceManager;
+  public IConnectionManager connectionManager;
+  public OlapMetaExplorer olapMetaExplorer;
+  public OlapDiscoverService olapDiscoverService;
+  public DatasourceService datasourceService;
+  public ThinQueryService thinQueryService;
   private List<String> data;
 
-  @Step
-  public void load() throws IOException {
-    AbstractServiceUtils ast = new AbstractServiceUtils();
-    ast.initTestContext();
-    IConnectionManager ic = new TConnectionManager();
-    String returned = computeTestDataRoot(DataSteps.class);
-    File f = new File(System.getProperty("java.io.tmpdir")+"/files/");
+
+  private void setup() throws Exception {
+    DatabaseHelper db = new DatabaseHelper();
+    db.setup();
+    InputStream inputStream = getClass().getResourceAsStream( "connection.properties" );
+    testProps.load( inputStream ); //$NON-NLS-1$
+
+
+    File f = new File( System.getProperty( "java.io.tmpdir" ) + "/files/" );
     f.mkdir();
-    IDatasourceManager ds = new ClassPathResourceDatasourceManager(System.getProperty("java.io.tmpdir")+"/files/");
-    InputStream inputStream= DataSteps.class.getResourceAsStream("connection.properties");
-    try {
-      testProps.load(inputStream);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    //this.datasourceManager = new ClassPathResourceDatasourceManager("res:saiku-datasources");
 
-    List<SaikuDatasource> l = new ArrayList<SaikuDatasource>(  );
+    this.datasourceManager =
+      new ClassPathResourceDatasourceManager( System.getProperty( "java.io.tmpdir" ) + "/files/" );
+    System.out.println( "Datasources: " + datasourceManager.getDatasources().keySet().size() );
+    //InputStream inputStream= DataSteps.class.getResourceAsStream("connection.properties");
+    testProps.load( inputStream );
 
-    if(data!=null) {
+    this.connectionManager = new SimpleConnectionManager();
+    this.connectionManager.setDataSourceManager( datasourceManager );
+    this.connectionManager.init();
+    this.olapMetaExplorer = new OlapMetaExplorer( connectionManager );
+    this.datasourceService = new DatasourceService();
+    this.datasourceService.setConnectionManager( connectionManager );
+    this.olapDiscoverService = new OlapDiscoverService();
+    this.olapDiscoverService.setDatasourceService( datasourceService );
+    this.thinQueryService = new ThinQueryService();
+    thinQueryService.setOlapDiscoverService( olapDiscoverService );
+  }
+
+  @Step
+  public void load() throws Exception {
+    setup();
+
+
+    List<SaikuDatasource> l = new ArrayList<SaikuDatasource>();
+
+    if ( data != null ) {
       for ( String s : data ) {
         l.add( new SaikuDatasource( s, SaikuDatasource.Type.OLAP, testProps ) );
       }
     }
 
-    ds.addDatasources( l );
+    datasourceManager.addDatasources( l );
 
-    ic.setDataSourceManager(ds);
-    olapMetaExplorer = new OlapMetaExplorer(ic);
   }
 
   @Step
-  public void createDataSources(List<String> data) {
+  public void createDataSources( List<String> data ) {
     this.data = data;
 
   }
 
   @Step
-  public void loadNewDataSources(){
-    List<SaikuDatasource> l = new ArrayList<SaikuDatasource>(  );
+  public void loadNewDataSources() throws Exception {
+    setup();
+    List<SaikuDatasource> l = new ArrayList<SaikuDatasource>();
 
-    if(data!=null) {
+    if ( data != null ) {
       for ( String s : data ) {
         l.add( new SaikuDatasource( s, SaikuDatasource.Type.OLAP, testProps ) );
       }
     }
-    IDatasourceManager ds = new ClassPathResourceDatasourceManager(System.getProperty("java.io.tmpdir")+"/files/");
 
-    ds.addDatasources( l );
-
-    IConnectionManager ic = new TConnectionManager();
-
-    ic.setDataSourceManager(ds);
-    olapMetaExplorer = new OlapMetaExplorer(ic);
+    datasourceManager.addDatasources( l );
   }
+
   @Step
-  public void loadsingle() throws IOException {
-    AbstractServiceUtils ast = new AbstractServiceUtils();
-    ast.initTestContext();
-    IConnectionManager ic = new TConnectionManager();
-    String returned = computeTestDataRoot(DataSteps.class);
-    File f = new File(System.getProperty("java.io.tmpdir")+"/files/");
-    f.mkdir();
-    IDatasourceManager ds = new ClassPathResourceDatasourceManager(System.getProperty("java.io.tmpdir")+"/files/");
-    InputStream inputStream= DataSteps.class.getResourceAsStream("connection.properties");
-    try {
-      testProps.load(inputStream);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+  public void loadsingle() throws Exception {
+    setup();
 
-    if(data!=null) {
+    if ( data != null ) {
 
-        ds.setDatasource( new SaikuDatasource( data.get( 0 ), SaikuDatasource.Type.OLAP, testProps ) );
+      datasourceManager.setDatasource( new SaikuDatasource( data.get( 0 ), SaikuDatasource.Type.OLAP, testProps ) );
 
     }
 
-    ic.setDataSourceManager(ds);
-    olapMetaExplorer = new OlapMetaExplorer(ic);
   }
 
   @Step
-  @Test(expected=SaikuServiceException.class)
-  public void createInvalidDataSources() throws IOException {
-    AbstractServiceUtils ast = new AbstractServiceUtils();
-    ast.initTestContext();
-    IConnectionManager ic = new TConnectionManager();
-    String returned = computeTestDataRoot(DataSteps.class);
-    File f = new File(System.getProperty("java.io.tmpdir")+"/files/");
-    f.mkdir();
-    IDatasourceManager ds = new ClassPathResourceDatasourceManager("/tmp/empty/");
-    InputStream inputStream= DataSteps.class.getResourceAsStream("connection.properties");
-    testProps.load(inputStream);
+  @Test( expected = SaikuServiceException.class )
+  public void createInvalidDataSources() throws Exception {
+    setup();
 
-
-    ds.setDatasource( new SaikuDatasource( "test", SaikuDatasource.Type.OLAP, testProps ) );
-
-
-
-    ic.setDataSourceManager(ds);
-    olapMetaExplorer = new OlapMetaExplorer(ic);
+    datasourceManager.setDatasource( new SaikuDatasource( "test", SaikuDatasource.Type.OLAP, testProps ) );
 
   }
 
   @Step
-  public SaikuConnection getConnection(String name) throws SaikuOlapException {
-    return olapMetaExplorer.getConnection(name);
+  public SaikuConnection getConnection( String name ) throws SaikuOlapException {
+    return olapMetaExplorer.getConnection( name );
 
   }
+
   @Step
   public List<SaikuConnection> getDatasources() throws SaikuOlapException {
     return olapMetaExplorer.getAllConnections();
   }
 
   @Step
-  @Test(expected=SaikuOlapException.class)
+  @Test( expected = SaikuOlapException.class )
   public SaikuConnection getInvalidDatasource( String nonexistant ) throws SaikuOlapException {
     try {
       return olapMetaExplorer.getConnection( nonexistant );
     } catch ( SaikuOlapException e ) {
       throw new SaikuOlapException( "Olap Exception" );
     }
-  }
-
-
-  public static String computeTestDataRoot(Class anyTestClass) throws IOException {
-
-    //create a temp file
-    File temp = File.createTempFile("temp-file-name", ".tmp");
-
-    System.out.println("Temp file : " + temp.getAbsolutePath());
-
-    //Get tempropary file path
-    String absolutePath = temp.getAbsolutePath();
-    String tempFilePath = absolutePath.
-      substring(0,absolutePath.lastIndexOf(File.separator));
-    return tempFilePath+"/";
   }
 
   @Step
