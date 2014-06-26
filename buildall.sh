b4ac25af82603964f09926f996805338817ae035
Tom Barber <tom@analytical-labs.com>
2014-06-26 10:37:19 +0100

    2.6 migration

--- buildall.sh
+++ buildall.sh
@@ -9,9 +9,9 @@ cd ..
 git submodule init
 git submodule update
 cd saiku-ui
-git checkout master 
+git checkout 2.6.x 
 git pull
-mvn clean package install:install-file -Dfile=target/saiku-ui-2.6-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.6-SNAPSHOT -Dpackaging=war
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.6.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.6 -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin
