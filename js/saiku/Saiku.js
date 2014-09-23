a9e3df44ad81ae830b650d4593ab7f1216ece971
Breno Polanski <breno.polanski@gmail.com>
2014-09-23 15:44:00 -0500

    Add function for loading CSS asynchronously

--- js/saiku/Saiku.js
+++ js/saiku/Saiku.js
@@ -77,6 +77,28 @@ var Saiku = {
             console.error(item);
         }
     },
+    loadCSS: function(href, media) {
+        var cssNode = window.document.createElement('link'),
+            ref = window.document.getElementsByTagName('script')[0];
+
+        cssNode.rel = 'stylesheet';
+        cssNode.href = href;
+
+        // Temporarily, set media to something non-matching to 
+        // ensure it'll fetch without blocking render
+        cssNode.media = 'only x';
+
+        // Inject link
+        ref.parentNode.insertBefore(cssNode, ref);
+
+        // Set media back to `all` so that the 
+        // stylesheet applies once it loads
+        setTimeout(function() {            
+            cssNode.media = media || 'all';
+        });
+
+        return cssNode;
+    },
     URLParams: {
         buildValue: function(value) {
             if (/^\s*$/.test(value))           { return null; }
