99d11235bfc0befaf798c09fcba48d3ff97c9720
breno.polanski <breno.polanski@gmail.com>
2014-09-26 21:54:48 -0300

    Add detect mobile browsers

--- js/saiku/Settings.js
+++ js/saiku/Settings.js
@@ -189,3 +189,9 @@ var isIE = (function(){
     return false;
 
 }());
+
+var isMobile = (function(userAgent) {
+  'use strict';
+
+  return !!userAgent.match(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i);
+}(navigator.userAgent));
