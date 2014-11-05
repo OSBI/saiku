0d68387c4d8bc4a1b2998e53b4678fcfbcb813dc
breno.polanski <breno.polanski@gmail.com>
2014-11-05 11:36:33 -0200

    SKU-1175 #comment Update interface date filter

--- js/saiku/views/DateFilterModal.js
+++ js/saiku/views/DateFilterModal.js
@@ -29,16 +29,16 @@ var DateFilterModal = Modal.extend({
 		'click a': 'call'
 	},
 
-	template_selection_operator: _.template(
-		'<div class="box_selections">' +
-			'<div class="available_selections">' +
+	template_selection: _.template(
+		'<div class="box-selections">' +
+			'<div class="available-selections">' +
 				'<div class="checkbox-selection">' +
 					'<input type="checkbox">' +
 				'</div>' +
 			'</div>' +
-			'<div class="available_selections">' +
+			'<div class="available-selections">' +
 				'<span class="i18n">Operator:</span><br>' +
-				'<div class="selection_options">' +
+				'<div class="selection-options">' +
 					'<div class="form-group-selection">' +
 						'<label><input type="radio"> Equals</label>' +
 					'</div>' +
@@ -63,38 +63,50 @@ var DateFilterModal = Modal.extend({
 					'<div class="form-group-selection">' +
 						'<label><input type="radio"> Not Between</label><br>' +
 					'</div>' +
-					// '<label>Select a date:</label>' +
-					// '<input type="text">' +
+				'</div>' +
+			'</div>' +
+		'</div>' +
+		'<div class="box-selections">' +
+			'<div class="clearfix"></div>' +
+			'<div class="available-selections">' +
+				'<div class="checkbox-selection">' +
+					'<input type="checkbox">' +
+				'</div>' +
+			'</div>' +
+			'<div class="available-selections">' +
+				'<span class="i18n">Fixed Date:</span><br>' +
+				'<div class="selection-options">' +
+					'<label><input type="radio">Today</label>' +
+					'<label><input type="radio">Yesterday</label>' +
+					'<label><input type="radio">Current Week</label>' +
+					'<label><input type="radio">Current Month</label><br>' +
+					'<label><input type="radio">Current Year</label>' +
+				'</div>' +
+			'</div>' +
+		'</div>' +
+		'<div class="box-selections">' +
+			'<div class="clearfix"></div>' +
+			'<div class="available-selections">' +
+				'<div class="checkbox-selection">' +
+					'<input type="checkbox">' +
+				'</div>' +
+			'</div>' +
+			'<div class="available-selections">' +
+				'<span class="i18n">Rolling Date:</span><br>' +
+				'<div class="selection-options">' +
+					'<select>' +
+						'<option>Last</option>' +
+						'<option>Next</option>' +
+					'</select>' +
+					'<select>' +
+						'<option>Day(s)</option>' +
+						'<option>Week(s)</option>' +
+						'<option>Month(s)</option>' +
+						'<option>Year(s)</option>' +
+					'</select>' +
 				'</div>' +
 			'</div>' +
 		'</div>'
-
-		// '<div class="available_selections">' +
-		// 	'<span class="i18n">Fixed Date:</span><br>' +
-		// 	'<div class="selection_options">' +
-		// 		'<label><input type="radio">Today</label>' +
-		// 		'<label><input type="radio">Yesterday</label>' +
-		// 		'<label><input type="radio">Current Week</label>' +
-		// 		'<label><input type="radio">Current Month</label><br>' +
-		// 		'<label><input type="radio">Current Year</label>' +
-		// 	'</div>' +
-		// '</div>' +
-
-		// '<div class="available_selections">' +
-		// 	'<span class="i18n">Rolling Date:</span><br>' +
-		// 	'<div class="selection_options">' +
-		// 		'<select>' +
-		// 			'<option>Last</option>' +
-		// 			'<option>Next</option>' +
-		// 		'</select>' +
-		// 		'<select>' +
-		// 			'<option>Day(s)</option>' +
-		// 			'<option>Week(s)</option>' +
-		// 			'<option>Month(s)</option>' +
-		// 			'<option>Year(s)</option>' +
-		// 		'</select>' +
-		// 	'</div>' +
-		// '</div>'
 	),
 
 	initialize: function(args) {
@@ -110,12 +122,12 @@ var DateFilterModal = Modal.extend({
 
 		// Load template
         this.$el.find('.dialog_body')
-        	.html(this.template_selection_operator);
+        	.html(this.template_selection);
 	},
 
     post_render: function(args) {
-        var left = ($(window).width() - 800)/2;
-        var width = $(window).width() < 800 ? $(window).width() : 800;
+        var left = ($(window).width() - 600)/2;
+        var width = $(window).width() < 600 ? $(window).width() : 600;
         $(args.modal.el).parents('.ui-dialog')
             .css({ width: width, left: "inherit", margin:"0", height: 490 })
             .offset({ left: left});
