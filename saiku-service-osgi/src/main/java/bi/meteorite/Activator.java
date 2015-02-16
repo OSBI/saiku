171ce126184ad01c71892cbd6743fa89acb2365b
Tom Barber <tom@analytical-labs.com>
2015-02-16 21:23:24 +0000

    fix

---
+++ saiku-service-osgi/src/main/java/bi/meteorite/Activator.java
@@ -0,0 +1,16 @@
+package bi.meteorite;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+        // TODO add activation code here
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // TODO add deactivation code here
+    }
+
+}
