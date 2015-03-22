fb428fd5f8b7b1c23dc2f0a5dde599a662e49f56
Bhagavan Das <bd@trivand.com>
2015-03-22 00:33:23 +0000

    Re-Instate the ThinQueryServiceTest and changed assertion to match mondrian4

--- saiku-core/saiku-service/src/test/java/org/saiku/service/olap/ThinQueryServiceTest.java
+++ saiku-core/saiku-service/src/test/java/org/saiku/service/olap/ThinQueryServiceTest.java
@@ -1,18 +1,17 @@
-/*
+
 package org.saiku.service.olap;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringWriter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.VFS;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.olap4j.Axis;
@@ -36,7 +35,6 @@ import org.saiku.query.mdx.NFilter;
 import org.saiku.query.metadata.CalculatedMeasure;
 import org.saiku.service.datasource.DatasourceService;
 
-
 public class ThinQueryServiceTest {
 
 	private TestSaikuContext context;
@@ -44,7 +42,6 @@ public class ThinQueryServiceTest {
 	private DatasourceService ds;
 	private ThinQueryService tqs;
 
-	
 	@Before
 	public void setUp() throws Exception {
 		context = TestSaikuContext.instance();
@@ -53,241 +50,204 @@ public class ThinQueryServiceTest {
 		tqs = context.thinQueryService;
 	}
 
-
 	@Test
-	public void testNewQuery() {
-		try {
-			SaikuCube c = TestSaikuContext.getSalesCube();
-			String name = "dummy";
-			ThinQuery tq = tqs.createEmpty(name, c);
-			ObjectMapper om = new ObjectMapper();
-			String query = om.defaultPrettyPrintingWriter().writeValueAsString(tq);
-			compareQuery(name, query);
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-
+	public void testNewQuery() throws Exception {
+		SaikuCube c = TestSaikuContext.getSalesCube();
+		String name = "dummy";
+		ThinQuery tq = tqs.createEmpty(name, c);
+		ObjectMapper om = new ObjectMapper();
+		String query = om.writerWithDefaultPrettyPrinter().writeValueAsString(tq);
+		compareQuery(name, query);
 	}
 
 	@Test
-	public void testQuery1() {
+	public void testQuery1() throws Exception {
+		SaikuCube c = TestSaikuContext.getSalesCube();
+		Cube cub = ods.getNativeCube(c);
+		String name = "query1";
+		Query query = new Query(name, cub);
+		QueryAxis columns = query.getAxis(Axis.COLUMNS);
+		QueryAxis rows = query.getAxis(Axis.ROWS);
+		QueryHierarchy products = query.getHierarchy("[Product].[Products]");
 
-		try  {
-			SaikuCube c = TestSaikuContext.getSalesCube();
-			Cube cub = ods.getNativeCube(c);
-			String name = "query1";
-			Query query = new Query(name, cub);
-			QueryAxis columns = query.getAxis(Axis.COLUMNS);
-			QueryAxis rows = query.getAxis(Axis.ROWS);
-			QueryHierarchy products = query.getHierarchy("[Product]");
+		products.includeLevel("Product Family");
+		products.excludeMember("[Products].[Non-Consumable]");
+		NFilter top2filter = new NFilter(MdxFunctionType.TopCount, 2, "Measures.[Unit Sales]");
+		products.addFilter(top2filter);
+		columns.addHierarchy(products);
 
-			products.includeLevel("Product Family");
-			products.excludeMember("[Product].[Non-Consumable]");
-			NFilter top2filter = new NFilter(MdxFunctionType.TopCount, 2, "Measures.[Unit Sales]");
-			products.addFilter(top2filter);
-			columns.addHierarchy(products);
+		QueryHierarchy edu = query.getHierarchy("[Customer].[Education Level]");
+		edu.includeLevel("Education Level");
+		columns.addHierarchy(edu);
 
-			QueryHierarchy edu = query.getHierarchy("[Education Level]");
-			edu.includeLevel("Education Level");
-			columns.addHierarchy(edu);
+		QueryHierarchy gender = query.getHierarchy("[Customer].[Gender]");
+		gender.includeMember("[Gender].[F]");
+		rows.addHierarchy(gender);
 
-			QueryHierarchy gender = query.getHierarchy("[Gender]");
-			gender.includeMember("[Gender].[F]");
-			rows.addHierarchy(gender);
+		CalculatedMeasure cm =
+				query.createCalculatedMeasure(
+						"Double Profit",
+						"( [Measures].[Store Sales] - [Measures].[Store Cost]) * 2",
+						null);
 
-			CalculatedMeasure cm =
-					query.createCalculatedMeasure(
-							"Double Profit", 
-							"( [Measures].[Store Sales] - [Measures].[Store Cost]) * 2",  
-							null);
+		columns.sort(SortOrder.BDESC, cm.getUniqueName());
 
+		query.getDetails().add(cm);
+		Measure m = cub.getMeasures().get(0);
 
-			columns.sort(SortOrder.BDESC, cm.getUniqueName());
+		query.getDetails().add(m);
 
-			query.getDetails().add(cm);
-			Measure m = cub.getMeasures().get(0);
+		ThinQuery tq = Thin.convert(query, c);
 
-			query.getDetails().add(m);
+		ObjectMapper om = new ObjectMapper();
+		String first = om.writerWithDefaultPrettyPrinter().writeValueAsString(tq);
 
-			ThinQuery tq = Thin.convert(query, c);
+		Query q2 = Fat.convert(tq, cub);
+		ThinQuery tq2 = Thin.convert(q2, c);
+		String second = om.writerWithDefaultPrettyPrinter().writeValueAsString(tq2);
 
-			ObjectMapper om = new ObjectMapper();
-			String first = om.defaultPrettyPrintingWriter().writeValueAsString(tq);
+		assertEquals(first, second);
+		compareQuery(name, second);
 
-			Query q2 = Fat.convert(tq, cub);
-			ThinQuery tq2 = Thin.convert(q2, c);
-			String second = om.defaultPrettyPrintingWriter().writeValueAsString(tq2);
-			assertEquals(first, second);
-			compareQuery(name, second);
+		String mdx = q2.getSelect().toString();
 
-			String mdx = q2.getSelect().toString();
+		String expectedMdx =
+				"WITH\n" +
+						"SET [~COLUMNS_Products] AS\n" +
+						"    TopCount(Except({[Product].[Products].[Product Family].Members}, {[Product].[Products].[Non-Consumable]}), 2, Measures.[Unit Sales])\n" +
+						"SET [~COLUMNS_Education Level] AS\n" +
+						"    {[Customer].[Education Level].[Education Level].Members}\n" +
+						"MEMBER [Measures].[Double Profit] AS\n" +
+						"    (([Measures].[Store Sales] - [Measures].[Store Cost]) * 2)\n" +
+						"SET [~ROWS] AS\n" +
+						"    {[Customer].[Gender].[F]}\n" +
+						"SELECT\n" +
+						"CrossJoin(Order(CrossJoin([~COLUMNS_Products], [~COLUMNS_Education Level]), [Measures].[Double Profit], BDESC), {[Measures].[Double Profit], [Measures].[Unit Sales]}) ON COLUMNS,\n" +
+						"[~ROWS] ON ROWS\n" +
+						"FROM [Sales]";
 
-			String expectedMdx = 
-					"WITH\n"
-			                + "SET [~COLUMNS_Product] AS\n"
-			                + "    TopCount(Except({[Product].[Product Family].Members}, {[Product].[Non-Consumable]}), 2, Measures.[Unit Sales])\n"
-			                + "SET [~COLUMNS_Education Level] AS\n"
-			                + "    {[Education Level].[Education Level].Members}\n"
-			                + "MEMBER [Measures].[Double Profit] AS\n"
-			                + "    (([Measures].[Store Sales] - [Measures].[Store Cost]) * 2)\n"
-			                + "SET [~ROWS] AS\n"
-			                + "    {[Gender].[F]}\n"
-			                + "SELECT\n"
-			                + "CrossJoin(Order(CrossJoin([~COLUMNS_Product], [~COLUMNS_Education Level]), [Measures].[Double Profit], BDESC), {[Measures].[Double Profit], [Measures].[Unit Sales]}) ON COLUMNS,\n"
-			                + "[~ROWS] ON ROWS\n"
-			                + "FROM [Sales]";
-			
-			assertEquals(expectedMdx, mdx);
+		assertEquals(expectedMdx, mdx);
 
-			CellSet cs = this.tqs.executeInternalQuery(tq2);
-			assertEquals("[ COLUMNS: 20 ][ ROWS: 1 ]", getResultInfo(cs));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-
-
+		CellSet cs = this.tqs.executeInternalQuery(tq2);
+		assertEquals("[ COLUMNS: 20 ][ ROWS: 1 ]", getResultInfo(cs));
 	}
 
 	@Test
-	public void testBasicParameter1() {
+	public void testBasicParameter1() throws Exception {
+		SaikuCube c = TestSaikuContext.getSalesCube();
+		Cube cub = ods.getNativeCube(c);
+		String name = "parameter1";
+		Query query = new Query(name, cub);
+		QueryAxis columns = query.getAxis(Axis.COLUMNS);
+		QueryAxis rows = query.getAxis(Axis.ROWS);
+		QueryHierarchy products = query.getHierarchy("[Product].[Products]");
+		products.includeLevel("Product Family");
+		products.excludeMember("[Products].[Non-Consumable]");
+		rows.addHierarchy(products);
 
-		try {
-			SaikuCube c = TestSaikuContext.getSalesCube();
-			Cube cub = ods.getNativeCube(c);
-			String name = "parameter1";
-			Query query = new Query(name, cub);
-			QueryAxis columns = query.getAxis(Axis.COLUMNS);
-			QueryAxis rows = query.getAxis(Axis.ROWS);
-			QueryHierarchy products = query.getHierarchy("[Product]");
-			products.includeLevel("Product Family");
-			products.excludeMember("[Product].[Non-Consumable]");
-			rows.addHierarchy(products);
+		QueryHierarchy edu = query.getHierarchy("[Customer].[Education Level]");
+		edu.includeLevel("Education Level");
+		columns.addHierarchy(edu);
 
-			QueryHierarchy edu = query.getHierarchy("[Education Level]");
-			edu.includeLevel("Education Level");
-			columns.addHierarchy(edu);
-
-			ThinQuery tq = Thin.convert(query, c);
-			ThinLevel tl = tq.getQueryModel().getAxis(AxisLocation.ROWS).getHierarchy("[Product]").getLevel("Product Family");
-			tl.getSelection().setParameterName("productFamilyList");
-			tq.setParameter("productFamilyList", "[Product].[Food]");
+		ThinQuery tq = Thin.convert(query, c);
+		ThinLevel tl = tq.getQueryModel().getAxis(AxisLocation.ROWS).getHierarchy("[Product].[Products]").getLevel("Product Family");
+		tl.getSelection().setParameterName("productFamilyList");
+		tq.setParameter("productFamilyList", "[Product].[Products].[Food]");
 
 
+		ObjectMapper om = new ObjectMapper();
+		String first = om.writerWithDefaultPrettyPrinter().writeValueAsString(tq);
 
-			ObjectMapper om = new ObjectMapper();
-			String first = om.defaultPrettyPrintingWriter().writeValueAsString(tq);
+		compareQuery(name, first);
 
+		Query q2 = Fat.convert(tq, cub);
 
-			compareQuery(name, first);
+		String mdx = q2.getSelect().toString();
 
-			Query q2 = Fat.convert(tq, cub);
+		String expectedMdx =
+				"WITH\n" +
+						"SET [~COLUMNS] AS\n" +
+						"    {[Customer].[Education Level].[Education Level].Members}\n" +
+						"SET [~ROWS] AS\n" +
+						"    Except({[Product].[Products].[Product Family].Members}, {[Product].[Products].[Food]})\n" +
+						"SELECT\n" +
+						"[~COLUMNS] ON COLUMNS,\n" +
+						"[~ROWS] ON ROWS\n" +
+						"FROM [Sales]";
 
-			String mdx = q2.getSelect().toString();
+		assertEquals(expectedMdx, mdx);
 
-			String expectedMdx = 
-					"WITH\n"
-							+ "SET [~COLUMNS] AS\n"
-							+ "    {[Education Level].[Education Level].Members}\n"
-							+ "SET [~ROWS] AS\n"
-							+ "    Except({[Product].[Product Family].Members}, {[Product].[Food]})\n"
-							+ "SELECT\n"
-							+ "[~COLUMNS] ON COLUMNS,\n"
-							+ "[~ROWS] ON ROWS\n"
-							+ "FROM [Sales]";
+		CellSet cs = tqs.executeInternalQuery(tq);
+		//		String s = TestUtil.toString(cs);
+		////			        System.out.println(TestUtil.toJavaString(s));
+		//		TestUtil.assertEqualsVerbose(
+		//				"Axis #0:\n"
+		//		                + "{}\n"
+		//		                + "Axis #1:\n"
+		//		                + "{[Education Level].[Bachelors Degree]}\n"
+		//		                + "{[Education Level].[Graduate Degree]}\n"
+		//		                + "{[Education Level].[High School Degree]}\n"
+		//		                + "{[Education Level].[Partial College]}\n"
+		//		                + "{[Education Level].[Partial High School]}\n"
+		//		                + "Axis #2:\n"
+		//		                + "{[Product].[Drink]}\n"
+		//		                + "{[Product].[Non-Consumable]}\n"
+		//		                + "Row #0: 6,423\n"
+		//		                + "Row #0: 1,325\n"
+		//		                + "Row #0: 7,226\n"
+		//		                + "Row #0: 2,164\n"
+		//		                + "Row #0: 7,459\n"
+		//		                + "Row #1: 13,051\n"
+		//		                + "Row #1: 2,990\n"
+		//		                + "Row #1: 14,929\n"
+		//		                + "Row #1: 4,522\n"
+		//		                + "Row #1: 14,744\n",
+		//						s);
+		assertEquals("[ COLUMNS: 5 ][ ROWS: 2 ]", getResultInfo(cs));
 
+		tq.setParameter("productFamilyList", "[Product].[Products].[Food],[Product].[Products].[Drink]");
+		q2 = Fat.convert(tq, cub);
+		mdx = q2.getSelect().toString();
 
-			assertEquals(expectedMdx, mdx);
+		expectedMdx =
+				"WITH\n" +
+						"SET [~COLUMNS] AS\n" +
+						"    {[Customer].[Education Level].[Education Level].Members}\n" +
+						"SET [~ROWS] AS\n" +
+						"    Except({[Product].[Products].[Product Family].Members}, {[Product].[Products].[Food], [Product].[Products].[Drink]})\n" +
+						"SELECT\n" +
+						"[~COLUMNS] ON COLUMNS,\n" +
+						"[~ROWS] ON ROWS\n" +
+						"FROM [Sales]";
 
-			CellSet cs = tqs.executeInternalQuery(tq);
-			//		String s = TestUtil.toString(cs);
-			////			        System.out.println(TestUtil.toJavaString(s));
-			//		TestUtil.assertEqualsVerbose(
-			//				"Axis #0:\n"
-			//		                + "{}\n"
-			//		                + "Axis #1:\n"
-			//		                + "{[Education Level].[Bachelors Degree]}\n"
-			//		                + "{[Education Level].[Graduate Degree]}\n"
-			//		                + "{[Education Level].[High School Degree]}\n"
-			//		                + "{[Education Level].[Partial College]}\n"
-			//		                + "{[Education Level].[Partial High School]}\n"
-			//		                + "Axis #2:\n"
-			//		                + "{[Product].[Drink]}\n"
-			//		                + "{[Product].[Non-Consumable]}\n"
-			//		                + "Row #0: 6,423\n"
-			//		                + "Row #0: 1,325\n"
-			//		                + "Row #0: 7,226\n"
-			//		                + "Row #0: 2,164\n"
-			//		                + "Row #0: 7,459\n"
-			//		                + "Row #1: 13,051\n"
-			//		                + "Row #1: 2,990\n"
-			//		                + "Row #1: 14,929\n"
-			//		                + "Row #1: 4,522\n"
-			//		                + "Row #1: 14,744\n",
-			//						s);
-			assertEquals("[ COLUMNS: 5 ][ ROWS: 2 ]", getResultInfo(cs));
+		assertEquals(expectedMdx, mdx);
 
-			tq.setParameter("productFamilyList", "[Product].[Food],[Product].[Drink]");
-			q2 = Fat.convert(tq, cub);
-			mdx = q2.getSelect().toString();
-
-			expectedMdx =
-					"WITH\n"
-							+ "SET [~COLUMNS] AS\n"
-							+ "    {[Education Level].[Education Level].Members}\n"
-							+ "SET [~ROWS] AS\n"
-							+ "    Except({[Product].[Product Family].Members}, {[Product].[Food], [Product].[Drink]})\n"
-							+ "SELECT\n"
-							+ "[~COLUMNS] ON COLUMNS,\n"
-							+ "[~ROWS] ON ROWS\n"
-							+ "FROM [Sales]";
-
-			assertEquals(expectedMdx, mdx);
-
-			cs = tqs.executeInternalQuery(tq);
-			assertEquals("[ COLUMNS: 5 ][ ROWS: 1 ]", getResultInfo(cs));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-
-
+		cs = tqs.executeInternalQuery(tq);
+		assertEquals("[ COLUMNS: 5 ][ ROWS: 1 ]", getResultInfo(cs));
 	}
 
 	@Test
-	public void testMdxQuery1() {
+	public void testMdxQuery1() throws Exception {
+		SaikuCube c = TestSaikuContext.getSalesCube();
+		String name = "mdx1";
+		String mdx = "SELECT Gender.Members on COLUMNS, Measures.Profit on ROWS from Sales";
+		ThinQuery tq = new ThinQuery(name, c, mdx);
+		ObjectMapper om = new ObjectMapper();
+		String first = om.writerWithDefaultPrettyPrinter().writeValueAsString(tq);
 
-		try {
-			SaikuCube c = TestSaikuContext.getSalesCube();
-			String name = "mdx1";
-			String mdx = "SELECT Gender.Members on COLUMNS, Measures.Profit on ROWS from Sales";
-			ThinQuery tq = new ThinQuery(name, c, mdx);
-			ObjectMapper om = new ObjectMapper();
-			String first = om.defaultPrettyPrintingWriter().writeValueAsString(tq);
+		compareQuery(name, first);
 
-			compareQuery(name, first);
+		CellSet cs = this.tqs.executeInternalQuery(tq);
+		assertEquals("[ COLUMNS: 3 ][ ROWS: 1 ]", getResultInfo(cs));
 
-			CellSet cs = this.tqs.executeInternalQuery(tq);
-			assertEquals("[ COLUMNS: 3 ][ ROWS: 1 ]", getResultInfo(cs));
+		Cube cube = ods.getNativeCube(c);
+		Query q = Fat.convert(tq, cube);
 
-			Cube cube = ods.getNativeCube(c);
-			Query q = Fat.convert(tq, cube);
-
-			//		File f = new File("/tmp/" + name + ".json");
-			//		FileWriter fw = new FileWriter(f);
-			//		fw.write(second);
-			//		fw.flush();
-			//		fw.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-
-
+		//		File f = new File("/tmp/" + name + ".json");
+		//		FileWriter fw = new FileWriter(f);
+		//		fw.write(second);
+		//		fw.flush();
+		//		fw.close();
 	}
 
 	private String getResultInfo(CellSet cs ) {
@@ -306,6 +266,4 @@ public class ThinQueryServiceTest {
 		String expected = stringWriter.toString();
 		assertEquals(expected, actual);
 	}
-
 }
-*/
