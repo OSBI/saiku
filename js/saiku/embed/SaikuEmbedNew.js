510ed3e0a22982d48de76f4bb7c18e87f9802089
Breno Polanski <breno.polanski@gmail.com>
2015-04-22 21:09:21 -0300

    Update SaikuEmbedNew.js

--- js/saiku/embed/SaikuEmbedNew.js
+++ js/saiku/embed/SaikuEmbedNew.js
@@ -1,17 +1,17 @@
-/**  
- * Copyright 2015 OSBI Ltd.
+/*  
+ *   Copyright 2015 OSBI Ltd
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 /**
@@ -141,12 +141,12 @@ var SaikuClient = (function() {
 	/**
 	 * Instance of SaikuTableRenderer and SaikuChartRenderer
 	 *
-	 * @property _SaikuRendererFactory
+	 * @property _saikuRendererFactory
 	 * @type {Object}
 	 * @private
 	 * @default { 'table': SaikuTableRenderer, 'chart': SaikuChartRenderer }
 	 */
-	var _SaikuRendererFactory = {
+	var _saikuRendererFactory = {
 		'table': SaikuTableRenderer,
 		'chart': SaikuChartRenderer
 	};
@@ -215,8 +215,8 @@ var SaikuClient = (function() {
 
 				options['mode'] = mode;
 
-				if (options.render in _SaikuRendererFactory) {
-					var saikuRenderer = new _SaikuRendererFactory[options.render](data, options);
+				if (options.render in _saikuRendererFactory) {
+					var saikuRenderer = new _saikuRendererFactory[options.render](data, options);
 					saikuRenderer.render();
 				}
 				else {
