e251c4e25d8a4e1af62671e402ad37cf5c9a53c6
Breno Polanski <breno.polanski@gmail.com>
2015-05-11 10:49:40 -0300

    Update SaikuEmbed.js

--- js/saiku/embed/SaikuEmbed.js
+++ js/saiku/embed/SaikuEmbed.js
@@ -1,21 +1,45 @@
+/*  
+ *   Copyright 2015 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 /**
  * Base 64 module
+ *
+ * @public
+ * @param  {window} window Window is passed through as local variable rather than global
+ * @return {String} Encoding data
  */
-;(function (window) {
+;(function(window) {
+	'use strict';
 
-	var
-		characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-		fromCharCode = String.fromCharCode,
-		INVALID_CHARACTER_ERR = (function () {
-			// fabricate a suitable error object
-			try { document.createElement('$'); }
-			catch (error) { return error; }}());
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+	var fromCharCode = String.fromCharCode;
+	var INVALID_CHARACTER_ERR = (function() {
+			// Fabricate a suitable error object
+			try { 
+				document.createElement('$'); 
+			}
+			catch(error) { 
+				return error; 
+			}
+		}());
 
-	// encoder
+	// Encoder
 	window.Base64 || (
-		window.Base64 = { encode: function (string) {
-			var
-				a, b, b1, b2, b3, b4, c, i = 0,
+		window.Base64 = { encode: function(string) {
+			var a, b, b1, b2, b3, b4, c, i = 0,
 				len = string.length, max = Math.max, result = '';
 
 			while (i < len) {
@@ -34,162 +58,254 @@
 
 				if (!b) {
 					b3 = b4 = 64;
-				} else if (!c) {
+				} 
+				else if (!c) {
 					b4 = 64;
 				}
 				result += characters.charAt(b1) + characters.charAt(b2) + characters.charAt(b3) + characters.charAt(b4);
 			}
+
 			return result;
 		}});
-
 }(this));
 
+/**
+ * IE Browser detection
+ *
+ * @public
+ * @return {Boolean} If `true` return the value of `v`, else return `false`
+ */
+var isIE = (function() {
+	'use strict';
 
-var isIE = (function(){
 	var undef, v = 3;
-
 	var dav = navigator.appVersion;
 
-	if(dav.indexOf('MSIE') != -1) {
+	if (dav.indexOf('MSIE') !== -1) {
 		v  = parseFloat(dav.split('MSIE ')[1]);
-		return v> 4 ? v : false;
+		return v > 4 ? v : false;
 	}
-	return false;
 
+	return false;
 }());
 
-if ($.blockUI && typeof Dashboards === "undefined") {
-	$.blockUI.defaults.css = { "font-size" : "14px"};
-	$.blockUI.defaults.overlayCSS = {};
-	$.blockUI.defaults.blockMsgClass = 'processing';
-	$.blockUI.defaults.fadeOut = 0;
-	$.blockUI.defaults.fadeIn = 0;
-	$.blockUI.defaults.ignoreIfBlocked = false;
-}
+/**
+ * A client for working with files Saiku
+ *
+ * @class
+ * @chainable
+ * @example
+ * 		var myClient = new SaikuClient({
+ * 			server: '/saiku',
+ * 			path: '/rest/saiku/embed',
+ * 			user: 'admin',
+ * 			password: 'admin'
+ * 		});
+ * @return {SaikuClient} The SaikuClient instance (for chaining)
+ */
+var SaikuClient = (function() {
+	'use strict';
 
-
-var SaikuConfig = {
-	server: null,
-	path: null,
-	user: null,
-	password: null
-};
-
-var SaikuCall = {
-	file: null,
-	render: 'table', // table | chart
-	mode: 'null', // table: sparkline, sparkbar - chart: line, bar, treemap, ...
-	formatter: 'flattened', // should be left unless you want an hierarchical resultset
-	htmlObject: "saiku",
-	zoom: true,
-	// table specific options for lazy loading table, doesn't quite work yet, so dont enable yet
-	/*
-	 batch:              true,
-	 batchSize:          1000,
-	 batchIntervalSize:  20,
-	 batchIntervalTime:  20
+	/**
+	 * The configuration settings for the request
+	 * 
+	 * @property _settings
+	 * @type {Object}
+	 * @private
+	 * @default { server: '/saiku', path: '/rest/saiku/embed', user: 'admin', password: 'admin', blockUI: false }
 	 */
-	params: {
-
-	}
-
-};
-
-var SaikuRendererFactory = {
-	"table" : SaikuTableRenderer,
-	"chart" : SaikuChartRenderer
-};
-
-var SaikuClient = function(config) {
-	this.config = _.extend(
-		SaikuConfig,
-		config
-	);
-};
-SaikuClient.prototype.error = function(jqXHR, textStatus, errorThrown) {
-	if (typeof console != "undefined" && console) {
-		console.error(textStatus);
-		console.error(jqXHR);
-		console.error(errorThrown);
-	}
-};
-
-SaikuClient.prototype.execute = function(usercall) {
-	var self = this;
-	var call = _.extend({},
-		SaikuCall,
-		usercall
-	);
-	if (typeof console != "undefined" && console) {
-		console.log(call);
-	}
-	var client = this.config;
-	var parameters = {};
-	if (call.params) {
-		for (key in call.params) {
-			parameters['param' + key] = call.params[key];
-		}
-	}
-	parameters = _.extend(
-		parameters,
-		{ "formatter" : call.formatter },
-		{ "file" : call.file }
-	);
-
-
-	if ($.blockUI && typeof Dashboards === "undefined") {
-		$(call.htmlObject).block({
-			message: '<span class="saiku_logo" style="float:left">&nbsp;&nbsp;</span> Executing....'
-		});
-	}
-	var params = {
-		// path ? "rest/saiku/embed/"
-		url:          client.server + (client.path ? client.path : "") + "/export/saiku/json",
-		type:         'GET',
-		cache:        false,
-		data:         parameters,
-		contentType:  'application/x-www-form-urlencoded',
-		dataType:     "json",
-		success:      function(data, textStatus, jqXHR) {
-			var renderMode = data.query.properties['saiku.ui.render.mode'] ? data.query.properties['saiku.ui.render.mode'] :  call.render;
-			var mode =  data.query.properties['saiku.ui.render.type'] ? data.query.properties['saiku.ui.render.type'] : call.mode;
-
-			//call['render'] = renderMode;
-			call['mode'] = mode;
-
-			if (call.render in SaikuRendererFactory) {
-				var r = new SaikuRendererFactory[call.render](data, call);
-				r.render();
-				if ($.blockUI) {
-					$(call.htmlObject).unblock();
-				}
-			} else {
-				alert('Render type ' + call.render + " not found!");
-			}
-			if ($.blockUI) {
-				$(call.htmlObject).unblock();
-			}
-		},
-		error:        function(jqXHR, textStatus, errorThrown) {
-			if ($.blockUI) {
-				$(call.htmlObject).unblock();
-			}
-
-			$(call.htmlObject).text("Error: " + textStatus);
-			self.error(jqXHR, textStatus, errorThrown);
-		},
-		crossDomain: true,
-		async:        true,
-		beforeSend:   function(request) {
-			if (client && client.user && client.password) {
-				var auth = "Basic " + Base64.encode(
-						client.user + ":" + client.password
-					);
-				request.setRequestHeader('Authorization', auth);
-				return true;
-			}
-		}
+	var _settings = {
+		server: '/saiku',
+		path: '/rest/saiku/embed',
+		user: 'admin',
+		password: 'admin',
+		blockUI: false
 	};
 
-	$.ajax(params);
-};
+	/**
+	 * The configuration options to render the file on page
+	 * 
+	 * @property _options
+	 * @type {Object}
+	 * @private
+	 * @default { file: null, render: 'table', mode: null, formatter: 'flattened', htmlObject: '#saiku', zoom: true, params: {} }
+	 */
+	var _options = {
+		file: null,
+		render: 'table', // table || chart
+		mode: null,      // table: sparkline, sparkbar || chart: line, bar, treemap, ...
+		formatter: 'flattened', // Should be left unless you want an hierarchical resultset
+		htmlObject: '#saiku',
+		zoom: true,
+		params: {}
+	};
+
+	/**
+	 * Instance of SaikuTableRenderer and SaikuChartRenderer
+	 *
+	 * @property _saikuRendererFactory
+	 * @type {Object}
+	 * @private
+	 * @default { 'table': SaikuTableRenderer, 'chart': SaikuChartRenderer }
+	 */
+	var _saikuRendererFactory = {
+		'table': SaikuTableRenderer,
+		'chart': SaikuChartRenderer
+	};
+	
+	/**
+	 * Add levels and parameter names
+	 * 
+	 * @private 
+	 * @param  {Object} dataAxis Axis FILTER, COLUMNS and ROWS with values in hierarchies
+	 * @return {Object} Levels and parameter names
+	 */
+	function joinParameters(dataAxis) {
+		var parametersLevels = [];
+
+		_.each(dataAxis, function(axis) {
+			_.each(axis, function(value) {
+				_.each(value.levels, function(levels) {
+					if (levels.selection.parameterName) {
+						parametersLevels.push({
+							levels: value.name + '.[' + levels.name + ']',
+							parameterName: levels.selection.parameterName
+						});
+					}
+				});
+			});
+		});
+
+		return parametersLevels;
+	}
+
+	/**
+	 * That constructor enforces the use of new, even if you call the constructor like a function
+	 *
+	 * @constructor
+	 * @private
+	 * @param  {Object} opts Settings for the request
+	 */
+	function SaikuClient(opts) {
+		// Enforces new
+		if (!(this instanceof SaikuClient)) {
+			return new SaikuClient(opts);
+		}
+
+		this.settings = _.extend(_settings, opts);
+	}
+
+	/**
+	 * Method for execute the requests of files Saiku
+	 *
+	 * @method execute
+ 	 * @public
+	 * @param  {Object} opts The configuration options to render the file on page
+	 * @example
+	 * 		myClient.execute({
+	 *   		file: '/homes/home:admin/report.saiku',
+	 *     		htmlObject: '#panel-body',
+	 *       	render: 'table',
+	 *      });
+	 */
+	SaikuClient.prototype.execute = function(opts) {
+		var self = this;
+		var options = _.extend({}, _options, opts);
+		var parameters = {};
+
+		if ($.blockUI && this.settings.blockUI) {
+			$.blockUI.defaults.css = { 'color': 'black', 'font-weight': 'normal' };
+			$.blockUI.defaults.overlayCSS = {};
+			$.blockUI.defaults.blockMsgClass = 'processing';
+			$.blockUI.defaults.fadeOut = 0;
+			$.blockUI.defaults.fadeIn = 0;
+			$.blockUI.defaults.ignoreIfBlocked = false;
+		}
+
+		if (options.params) {
+			for (var key in options.params) {
+				if (options.params.hasOwnProperty(key)) {
+					parameters['param' + key] = options.params[key];
+				}
+			}
+		}
+
+		parameters = _.extend(
+			parameters,
+			{ 'formatter': options.formatter },
+			{ 'file': options.file }
+		);
+
+		if ($.blockUI && this.settings.blockUI) {
+			$(options.htmlObject).block({
+				message: '<span class="saiku_logo" style="float:left">&nbsp;&nbsp;</span> Executing....'
+			});
+		}
+
+		var params = {
+			url: self.settings.server + (self.settings.path ? self.settings.path : '') + '/export/saiku/json',
+			type: 'GET',
+			cache: false,
+			data: parameters,
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			crossDomain: true,
+			async: true,
+			beforeSend: function(request) {
+				if (self.settings.user && self.settings.password) {
+					var auth = 'Basic ' + Base64.encode(
+							self.settings.user + ':' + self.settings.password
+						);
+					request.setRequestHeader('Authorization', auth);
+					return true;
+				}
+			},
+			success: function(data, textStatus, jqXHR) {
+				var renderMode = data.query.properties['saiku.ui.render.mode'] ? data.query.properties['saiku.ui.render.mode'] : options.render;
+				var mode = data.query.properties['saiku.ui.render.type'] ? data.query.properties['saiku.ui.render.type'] : options.mode;
+				var dataAxis = {
+					dataFilter: data.query.queryModel.axes.FILTER['hierarchies'],
+					dataColumns: data.query.queryModel.axes.COLUMNS['hierarchies'],
+					dataRows: data.query.queryModel.axes.ROWS['hierarchies']
+				};
+				var parametersValues = data.query.parameters;
+				var parametersLevels;
+
+				if (self.settings.dashboards) {
+					parametersLevels = joinParameters(dataAxis);
+					$(options.htmlObject).closest('.gs-w').data('parametersLevels', JSON.stringify(parametersLevels));
+					$(options.htmlObject).closest('.gs-w').data('parametersValues', JSON.stringify(parametersValues));
+				}
+
+				options['mode'] = mode;
+
+				if (options.render in _saikuRendererFactory) {
+					var saikuRenderer = new _saikuRendererFactory[options.render](data, options);
+					saikuRenderer.render();
+					if ($.blockUI) {
+						$(options.htmlObject).unblock();
+					}
+				}
+				else {
+					alert('Render type ' + options.render + ' not found!');
+				}
+				if ($.blockUI) {
+					$(options.htmlObject).unblock();
+				}
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				if ($.blockUI) {
+					$(options.htmlObject).unblock();
+				}
+				$(options.htmlObject).text('Error: ' + textStatus);
+				console.error(textStatus);
+				console.error(jqXHR);
+				console.error(errorThrown);
+			}
+		};
+
+		$.ajax(params);
+	};
+
+	return SaikuClient;
+}());
