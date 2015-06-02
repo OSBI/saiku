81e1dadd9d3ad33e360985a7b6ee4c78b1faf43c
Andreas Verleysen <andreas.verleysen@conundra.eu>
2015-06-02 15:38:49 +0200

    ChangeLocale: AdminResource updateLocale method uses datasource name instead of ID

--- saiku-core/saiku-web/src/main/java/org/saiku/web/rest/resources/AdminResource.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/rest/resources/AdminResource.java
@@ -223,22 +223,22 @@ public class AdminResource {
     /**
      * Updates the locale parameter of the datasource
      * @param locale: the new locale for the data source
-     * @param id: ID of the data source whose locale should be changed
+     * @param datasourceName: ID of the data source whose locale should be changed
      * @return: Response indicating success or fail
      */
     @PUT
     @Produces({"application/json"})
     @Consumes({"application/json"})
-    @Path("/datasources/{id}/locale")
+    @Path("/datasources/{datasourceName}/locale")
     @ReturnType("org.saiku.web.rest.objects.DataSourceMapper")
-    public Response updateDatasourceLocale(String locale, @PathParam("id") String id) {
+    public Response updateDatasourceLocale(String locale, @PathParam("datasourceName") String datasourceName) {
         try {
             boolean overwrite = true;
-            SaikuDatasource saikuDatasource = datasourceService.fetchDataSourceById(id);
+            SaikuDatasource saikuDatasource = datasourceService.getDatasource(datasourceName);
             datasourceService.setLocaleOfDataSource(saikuDatasource, locale);
             datasourceService.addDatasource(saikuDatasource, overwrite);
             return Response.ok().type("application/json").entity(new DataSourceMapper(saikuDatasource)).build();
-        }catch(SaikuDataSourceException e){
+        } catch(SaikuDataSourceException e){
             return Response.ok().type("application/json").entity(e.getLocalizedMessage()).build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getLocalizedMessage())
