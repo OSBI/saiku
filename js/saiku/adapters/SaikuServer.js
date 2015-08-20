ba35b6299f4cd7641e3c28cbb25eccc3d8e34322
breno.polanski <breno.polanski@gmail.com>
2015-08-20 17:09:44 -0300

    Create branch development

---
+++ js/saiku/adapters/SaikuServer.js
@@ -0,0 +1,178 @@
+/*  
+ *   Copyright 2012 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+ 
+/**
+ * Base 64 module
+ */
+
+;(function (window) {
+  /*jshint -W030 */
+  var
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+    fromCharCode = String.fromCharCode,
+    INVALID_CHARACTER_ERR = (function () {
+      // fabricate a suitable error object
+      try { document.createElement('$'); }
+      catch (error) { return error; }}());
+
+  // encoder
+  window.Base64 || (
+  window.Base64 = { encode: function (string) {
+    var
+      a, b, b1, b2, b3, b4, c, i = 0,
+      len = string.length, max = Math.max, result = '';
+
+    while (i < len) {
+      a = string.charCodeAt(i++) || 0;
+      b = string.charCodeAt(i++) || 0;
+      c = string.charCodeAt(i++) || 0;
+
+      if (max(a, b, c) > 0xFF) {
+        throw INVALID_CHARACTER_ERR;
+      }
+
+      b1 = (a >> 2) & 0x3F;
+      b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
+      b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
+      b4 = c & 0x3F;
+
+      if (!b) {
+        b3 = b4 = 64;
+      } else if (!c) {
+        b4 = 64;
+      }
+      result += characters.charAt(b1) + characters.charAt(b2) + characters.charAt(b3) + characters.charAt(b4);
+    }
+    return result;
+  }});
+
+}(this));
+
+/**
+ * Model which handles AJAX calls to the Saiku Server
+ * If you want to hook the UI up to something besides the Saiku Server,
+ * this is the class which you want to override.
+ * @returns {SaikuServer}
+ */
+Backbone.sync = function(method, model, options) {
+    var params;
+    methodMap = {
+        'create': "POST",
+        'read': "GET",
+        'update': "PUT",
+        'delete': "DELETE"
+    };
+    
+    // Generate AJAX action
+    var type = methodMap[method];
+    var url = Settings.REST_URL + (_.isFunction(model.url) ? model.url() : model.url);
+    
+    // Prepare for failure
+    if (typeof Settings.ERRORS == "undefined") {
+        Settings.ERRORS = 0;
+    }
+
+    var errorLogout = function() {
+        Settings.ERRORS++;
+        if (Settings.ERRORS < Settings.ERROR_TOLERANCE) {
+          Saiku.session.logout();
+        } else {
+          Saiku.ui.block("Communication problem with the server. Please reload the application...");
+        }
+    };
+    var statuscode = {
+      0: function() {
+        errorLogout();
+      },
+      401: function() {
+        errorLogout();
+      }
+    };
+
+    var failure = function(jqXHR, textStatus, errorThrown) {
+      if (!isIE && typeof console != "undefined" && console && console.error) {
+        console.error("Error performing " + type + " on " + url);
+        console.error(errorThrown);
+      }
+      if (options.error) {
+        options.error(jqXHR, textStatus, errorThrown);
+      }
+    };
+
+    var success = function(data, textStatus, jqXHR) {
+      Settings.ERRORS = 0;
+      Saiku.ui.unblock();  
+      options.success(data, textStatus, jqXHR);
+    };
+    var async = true;
+    if (options.async === false) {
+      async = false;
+    }
+    var dataType = 'json';
+    if (typeof options.dataType != "undefined") {
+      dataType = options.dataType;
+    }
+
+    var contentType = 'application/x-www-form-urlencoded';
+    if (typeof options.contentType != "undefined") {
+      contentType = options.contentType;
+    }
+    var data = model.attributes;
+    if (typeof options.data != "undefined") {
+      data = options.data;
+    }
+    // Default JSON-request options.
+    params = {
+      url:          url,
+      type:         type,
+      cache:        false,
+      data:         data,
+      contentType:  contentType,
+      dataType:     dataType,
+      success:      success,
+      statusCode:   statuscode, 
+      error:        failure,
+      async:        async//,
+      //processData:  false
+      /*
+      beforeSend:   function(request) {
+        if (!Settings.PLUGIN) {
+          var auth = "Basic " + Base64.encode(
+              Saiku.session.username + ":" + Saiku.session.password
+          );
+          request.setRequestHeader('Authorization', auth);
+          }
+      } */
+    };
+
+    if(options.processData === false){
+        params.processData = false;
+    }
+    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+    // And an `X-HTTP-Method-Override` header.
+    if ((Settings.BIPLUGIN && !Settings.BIPLUGIN5) || Backbone.emulateHTTP) {
+      if (type === 'PUT' || type === 'DELETE') {
+        if (Backbone.emulateHTTP) params.data._method = type;
+        params.type = 'POST';
+        params.beforeSend = function(xhr) {
+          xhr.setRequestHeader("X-HTTP-Method-Override", type);
+        };
+      }
+    }
+
+    // Make the request
+    $.ajax(params);
+};
---
+++ js/saiku/views/PermissionsModal.js
@@ -0,0 +1,157 @@
+/*  
+ *   Copyright 2012 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+ 
+/**
+ * Dialog for member selections
+ */
+var PermissionsModal = Modal.extend({
+    type: "permissions",
+    
+    buttons: [
+        { text: "Ok", method: "ok" },
+        { text: "Cancel", method: "close" }
+    ],
+
+    events: {
+        'click .add_role': 'add_role',
+        'click .remove_acl' : 'remove_acl',
+        'submit form': 'add_role',
+        'click a': 'call',
+        'click input.private' : 'keep_private'
+
+    },
+
+    rolesacl : {},
+    acltype: "SECURED",
+
+    
+    initialize: function(args) {
+        // Initialize properties
+        _.extend(this, args);
+        this.options.title = args.title;
+        this.file = args.file;
+        this.rolesacl = {};
+        Saiku.ui.unblock();
+        _.bindAll(this, "ok", "add_role", "remove_acl");
+
+        // Resize when rendered
+        this.bind('open', Saiku.i18n.translate());
+        this.render();
+               // Load template
+       $(this.el).find('.dialog_body')
+          .html(_.template($("#template-permissions").html())(this));
+
+        $(this.el).find('.filterbox').autocomplete({
+                    minLength: 1,
+                    source: Saiku.session.roles
+                }).data( "autocomplete" );
+        /*._renderItem = function( ul, item ) {
+                return $( "<li></li>" )
+                    .data( "item.autocomplete", item )
+                    .append( "<a class='label'>" + item.label + "</a>" )
+                    .appendTo( ul );
+                };
+*/
+        var acl = new RepositoryAclObject({ file : this.file });
+        acl.fetch({ async: false });
+
+        var definedRoles = (typeof acl.get('roles') == "undefined" || acl.get('roles') === null ? {} : acl.get('roles')); 
+        this.rolesacl = definedRoles;
+        var templ_roles =_.template($("#template-permissions-rolelist").html())({roles: definedRoles });
+
+
+        $(this.el).find('.rolelist').html(templ_roles);
+
+        var owner = (typeof acl.get('owner') == "undefined" || acl.get('owner') === null ? "" : acl.get('owner')); 
+        var atype = (typeof acl.get('type') == "undefined" || acl.get('type') === null ? null : acl.get('type')); 
+        if (atype !== null && atype == "PRIVATE") {
+            $(this.el).find('.private_owner .owner').text(owner);
+            $(this.el).find('.private_owner').show();
+        }
+        $(this.el).find(".i18n").i18n(Saiku.i18n.po_file);
+    },
+    
+    add_role: function(event) {
+        var self = this;
+        event.preventDefault();
+        if (this.acltype == "PRIVATE") {
+            return false;
+        }
+        var role = $(this.el).find(".filterbox").val();
+        var acls = [];
+        var aclstring ="";
+        var rolecount = 0;
+        if (role && role.length > 0) {
+            $(this.el).find('.acl:checked').each( function(index) { 
+                if (index > 0) {
+                    aclstring += ", ";
+                }
+                rolecount++;
+                aclstring += $(this).val();
+                acls.push($(this).val());
+            });
+            if (rolecount > 0) {
+                self.rolesacl[role] = acls;
+                $("<option value='" + role + "'>" + role + " ["+aclstring+"]</option>").appendTo($(this.el).find(".select_roles"));
+                role = $(this.el).find(".filterbox").val("");
+            } else {
+                alert("You need to chose at least one ACL method for this role.");
+            }
+        }
+        
+
+        return false;
+    },
+
+    keep_private: function(event) {
+        var isPrivate = $(this.el).find('input.private').is(':checked');
+        if (isPrivate) {
+            $(this.el).find('.permissions').addClass('disabled_toolbar');
+            $("input.acl, input.filterbox, input.add_role, input.remove_acl").prop('disabled', true);
+            this.acltype = "PRIVATE";
+        } else {
+            $(this.el).find('.permissions').removeClass('disabled_toolbar');
+            $("input.acl, input.filterbox, input.add_role, input.remove_acl").prop('disabled', false);
+            this.acltype = "SECURED";
+        }
+    },
+
+    remove_acl: function(event) {
+        var self = this;
+        if (this.acltype == "PRIVATE") {
+            return false;
+        }
+
+        $(this.el).find(".select_roles option:selected").each( function(index) { 
+            delete self.rolesacl[$(this).val()];
+        });
+        $(this.el).find(".select_roles option:selected").remove();
+        return false;
+    },
+
+    ok: function() {
+        var closer = this.close();
+        var acl = {};
+        if (this.acltype == "PRIVATE") {
+            acl = { "type" : "PRIVATE", "owner" : Saiku.session.username };
+        } else {
+            acl = { "type" : "SECURED", "roles" : this.rolesacl, "owner" : Saiku.session.username };
+        }
+        (new RepositoryAclObject({ file: this.file, acl: JSON.stringify(acl)})).save({ success: closer });
+
+        return false;
+    }
+});
