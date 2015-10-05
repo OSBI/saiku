c045e89033e4652d96a1914d12139af21fb39955
Tom Barber <tom@analytical-labs.com>
2015-10-05 11:29:46 +0100

    update license stuff

--- js/saiku/views/AboutModal.js
+++ js/saiku/views/AboutModal.js
@@ -26,10 +26,70 @@ var AboutModal = Modal.extend({
 
     message: Settings.VERSION + '<br>' +
         '<a href="http://saiku.meteorite.bi" target="_blank">http://saiku.meteorite.bi</a><br><br>' +
-        'Powered by <img src="images/src/meteorite_free.png" width="20px"> <a href="http://www.meteorite.bi/consulting/" target="_blank">www.meteorite.bi</a>',
+        '<h2>License Type</h2>'+
+        '<span class="licensetype"/> - Expires: <span class="licenseexpr"/><br/>'+
+        'Number of users: <span class="licenseuserlimit"/><br/>'+
+        'Licensed to: <span class="licensename"/> - <span class="licenseemail"/>'+
+        '<h2>Unlicenced User Quota</h2><br/>'+
+        '<div id="licensetable">'+
+        '<div class="table-wrapper">'+
+        '<div class="table-scroll">'+
+        '<table>'+
+        '<thead>'+
+        '<tr>'+
+        '<th><span class="text">Username</span></th>'+
+        '<th><span class="text">Logins Remaining</span></th>'+
+        '</tr>'+
+        '</thead>'+
+        '<tbody>'+
+        '<tr id="quotareplace"/>'+
+        '</tbody>'+
+        '</table>'+
+        '</div>'+
+        '</div>'+
+        '</div>'+
+        '<strong><a href="http://sites.fastspring.com/meteoriteconsulting/product/saikusilver" target="_blank">Order more licenses' +
+        ' here</a></strong><br/>'+
+        'Powered by <img src="images/src/meteorite_free.png" width="20px"> <a href="http://www.meteorite.bi/consulting/" target="_blank">www.meteorite.bi</a><br/>',
 
     initialize: function() {
         this.options.title = 'About ' + Settings.VERSION;
+    },
+
+    render: function() {
+        $(this.el).html(this.template())
+            .addClass("dialog_" + this.type)
+            .dialog(this.options);
+
+        var uiDialogTitle = $('.ui-dialog-title');
+        uiDialogTitle.html(this.options.title);
+        uiDialogTitle.addClass('i18n');
+        Saiku.i18n.translate();
+        license = new License();
+
+        yourEpoch = parseFloat(Settings.LICENSE.expiration);
+        var yourDate = new Date(yourEpoch);
+
+        $(this.el).find(".licensetype").text(Settings.LICENSE.licenseType);
+        $(this.el).find(".licenseexpr").text(yourDate.toLocaleDateString());
+        $(this.el).find(".licensename").text(Settings.LICENSE.name);
+        $(this.el).find(".licenseuserlimit").text(Settings.LICENSE.userLimit);
+        $(this.el).find(".licenseemail").text(Settings.LICENSE.email);
+        var tbl_body = "";
+        var odd_even = false;
+        $.each(Settings.LICENSEQUOTA, function() {
+            var tbl_row = "";
+            $.each(this, function(k , v) {
+                tbl_row += "<td>"+v+"</td>";
+            });
+            tbl_body += "<tr class=\""+( odd_even ? "odd" : "even")+"\">"+tbl_row+"</tr>";
+            odd_even = !odd_even;
+        });
+
+        $(this.el).find("#quotareplace").replaceWith(tbl_body);
+
+
+        return this;
     },
 
     close: function(event) {
