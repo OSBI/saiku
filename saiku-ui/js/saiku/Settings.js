f04713f5ebd4999c56afcf94abfd99853ba8ca0f
brenopolanski <breno.polanski@gmail.com>
2016-08-09 15:48:14 -0300

    SE-426 #comment If a fact table has columns named measure_ or m_ we should assume these to be measures

--- saiku-ui/js/saiku/Settings.js
+++ saiku-ui/js/saiku/Settings.js
@@ -129,9 +129,9 @@ var Settings = {
 	},
     SCHEMA_EDITOR: {
         // The `^` matches beginning of input.
-        STAR_SCHEMA_FACT_TABLE: /^fact_|f_/i,
-        STAR_SCHEMA_MEASURE_TABLE: /^measure_|m_/i,
-        STAR_SCHEMA_DIMENSION_TABLE: /^dimension_|dim_|d_/i
+        STAR_SCHEMA_FACT_TABLE: /^fact_|^f_/i,
+        STAR_SCHEMA_DIMENSION_TABLE: /^dimension_|^dim_|^d_/i,
+        STAR_SCHEMA_MEASURE_COLUMN: /^measure_|^m_/i
     }
 };
 
