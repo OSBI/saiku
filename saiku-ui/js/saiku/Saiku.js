875c1a2397985aedcc19eb9e856e4f25a80893e8
brenopolanski <breno.polanski@gmail.com>
2017-04-21 19:03:23 -0300

    Refactor Saiku.js

--- saiku-ui/js/saiku/Saiku.js
+++ saiku-ui/js/saiku/Saiku.js
@@ -28,8 +28,14 @@ var Saiku = {
      */
     tabs: new TabSet(),
 
-    introdone: false,
+    /**
+     * Stop the intro plugin when it finishes
+     */
+    introDone: false,
 
+    /**
+     * Saiku SplashScreen
+     */
     splash: new SplashScreen({ toolbar: this.toolbar }),
 
     /**
@@ -58,33 +64,58 @@ var Saiku = {
     ui: {
         block: function(message) {
             $('.processing_message').html(message);
-            $('.processing_message').removeClass("i18n_translated").addClass("i18n");
+            $('.processing_message').removeClass('i18n_translated').addClass('i18n');
+
             Saiku.i18n.translate();
 
-            $('.processing,.processing_container').show();
+            $('.processing, .processing_container').show();
         },
 
         unblock: function() {
-            $('.processing,.processing_container, .blockOverlay').hide();
+            $('.processing, .processing_container, .blockOverlay').hide();
 
-            // Fix For Internet Explorer 10 UIBlock issue
+            // Fix for Internet Explorer 10 UIBlock issue
             $('.blockUI').fadeOut('slow');
         }
     },
+
+    /**
+     * Outputs a message to the Web Console
+     */
     log: function(channel, item) {
         if (console && console.log) {
-            console.log("Logging for: " + channel);
+            console.log('Logging for: ' + channel);
+
             if (item) {
                 console.log(item);
             }
         }
     },
+
+    /**
+     * Outputs an error message to the Web Console
+     */
     error: function(channel, item) {
         if (console && console.error) {
-            console.error("Logging for: " + channel);
+            console.error('Logging for: ' + channel);
             console.error(item);
         }
     },
+
+    /**
+     * Defines utility methods to work with the query string of a URL
+     *
+     * @example:
+     *
+     *    Put in your URL:
+     *    http://<YOUR_MACHINE_IP>:8080/?splash=false
+     *
+     *    var paramsURI = Saiku.URLParams.paramsURI();
+     *
+     *    if (Saiku.URLParams.contains({ splash: paramsURI.splash })) {
+     *        // Do something...
+     *    }
+     */
     URLParams: {
         buildValue: function(value) {
             if (/^\s*$/.test(value))           { return null; }
@@ -156,9 +187,17 @@ var Saiku = {
             }
         }
     },
+
+    /**
+     * A function for loading CSS asynchronously
+     *
+     * @example:
+     *
+     *    Saiku.loadCSS('PATH_OF_YOUR_CSS');
+     */
     loadCSS: function(href, media) {
-        var cssNode = window.document.createElement('link'),
-            ref = window.document.getElementsByTagName('script')[0];
+        var cssNode = window.document.createElement('link');
+        var ref = window.document.getElementsByTagName('script')[0];
 
         cssNode.rel = 'stylesheet';
         cssNode.href = href;
@@ -178,9 +217,17 @@ var Saiku = {
 
         return cssNode;
     },
+
+    /**
+     * A function for asynchronously loading JavaScript files
+     *
+     * @example:
+     *
+     *    Saiku.loadJS('PATH_OF_YOUR_JS');
+     */
     loadJS: function(src, callback) {
-        var scriptNode = window.document.createElement('script'),
-            ref = window.document.getElementsByTagName('script')[0];
+        var scriptNode = window.document.createElement('script');
+        var ref = window.document.getElementsByTagName('script')[0];
 
         scriptNode.src = src;
         scriptNode.async = true;
@@ -195,17 +242,27 @@ var Saiku = {
 
         return scriptNode;
     },
-    toPattern: function(value, opts) {
-        var DIGIT = '9',
-            ALPHA = 'A',
-            ALPHANUM = 'S',
-            output = (typeof opts === 'object' ? opts.pattern : opts).split(''),
-            values = value.toString().replace(/[^0-9a-zA-Z]/g, ''),
-            index = 0,
-            len = output.length,
-            i;
 
-        for (i = 0; i < len; i++) {
+    /**
+     * Converts value using a mask
+     *
+     * @example:
+     *
+     *    Saiku.toPattern(1099911111, '(99) 9999-9999');  // -> (10) 9991-1111
+     *    Saiku.toPattern(12122000, '99/99/9999');        // -> 12/12/2000
+     *    Saiku.toPattern(99911111101, '999.999.999-99'); // -> 999.111.111-01
+     *    Saiku.toPattern('ABC1234', 'AAA-9999');         // -> ABC-1234
+     */
+    toPattern: function(value, opts) {
+        var DIGIT = '9';
+        var ALPHA = 'A';
+        var ALPHANUM = 'S';
+        var output = (typeof opts === 'object' ? opts.pattern : opts).split('');
+        var values = value.toString().replace(/[^0-9a-zA-Z]/g, '');
+        var index = 0;
+        var len = output.length;
+
+        for (var i = 0; i < len; i++) {
             if (index >= values.length) {
                 break;
             }
@@ -223,6 +280,14 @@ var Saiku = {
 
         return output.join('').substr(0, i);
     },
+
+    /**
+     * Converts value using a mask
+     *
+     * @example:
+     *
+     *    Saiku.replaceString('World', 'Web', 'Saiku Analytics World'); // -> Saiku Analytics Web
+     */
     replaceString: function(oldS, newS, fullS) {
         var len = fullS.length;
 
@@ -234,11 +299,29 @@ var Saiku = {
 
         return fullS;
     },
+
+    /**
+     * Remove brackets
+     *
+     * @example:
+     *
+     *    Saiku.removeBrackets('[Time].[Time].[Year]'); // -> Time.Time.Year
+     */
     removeBrackets: function(value) {
         var str = value.toString();
 
         return str.replace(/[\[\]]/gi, '');
     },
+
+    /**
+     * Trim first or last char or both
+     *
+     * @example:
+     *
+     *    Saiku.trimFirstLastChar('Saiku Analytics', 'first'); // -> aiku Analytics
+     *    Saiku.trimFirstLastChar('Saiku Analytics', 'last');  // -> Saiku Analytic
+     *    Saiku.trimFirstLastChar('Saiku Analytics');          // -> aiku Analytic
+     */
     trimFirstLastChar: function(value, trimPosChar) {
         var str = value.toString();
 
@@ -291,144 +374,146 @@ Saiku.singleton = (function() {
 Backbone.emulateHTTP = false;
 
 /**
- * Up up and away!
- */
-if (! Settings.BIPLUGIN) {
-    $(document).ready(function () {
-        var plugins = new PluginCollection();
-
-
-            plugins.fetch({
-                success: function () {
-                    var settingsoverride = new SettingsOverrideCollection();
-
-                    settingsoverride.fetch({
-                        success: function () {
-                            var i = plugins.size();
-                            var j = 0;
-                            plugins.each(function (log) {
-                                j = j + 1;
-                                if (log.attributes.path != "js/saiku/plugins/I18n/plugin.js") {
-                                    jQuery.ajax({
-                                        async: false,
-                                        type: 'GET',
-                                        url: log.attributes.path,
-                                        data: null,
-                                        success: function () {
-                                            if (j == i) {
-
-                                                var k = settingsoverride.size();
-                                                var l = 0;
-                                                settingsoverride.each(function (log) {
-                                                    l = l + 1;
-
-                                                    for (var key in log.attributes) {
-                                                        Settings[key] = log.attributes[key];
-                                                    }
-                                                    if (Settings.CSS != undefined) {
-                                                        Saiku.loadCSS(Settings.CSS, null)
-                                                    }
-                                                    if (k == l) {
-                                                        Saiku.session = new Session({}, {
-                                                            username: Settings.USERNAME,
-                                                            password: Settings.PASSWORD
-                                                        });
-
-                                                        Saiku.toolbar = new Toolbar();
-                                                    }
-                                                });
-
-                                            }
-                                        },
-                                        dataType: 'script'
-                                    });
-                                }
-                                else {
-                                    if (j == i) {
-
-                                        var k = settingsoverride.size();
-                                        var l = 0;
-                                        settingsoverride.each(function (log) {
-                                            l = l + 1;
-
-                                            for (var key in log.attributes) {
-                                                Settings[key] = log.attributes[key];
-                                            }
-                                            if (Settings.CSS != undefined) {
-                                                Saiku.loadCSS(Settings.CSS, null)
-                                            }
-                                            if (k == l) {
-                                                Saiku.session = new Session({}, {
-                                                    username: Settings.USERNAME,
-                                                    password: Settings.PASSWORD
-                                                });
-
-                                                    Saiku.toolbar = new Toolbar();
-                                            }
-                                        });
-
-                                    }
-                                }
-
-                            });
-
-
-                        },
-                        error: function () {
-                            var i = plugins.size();
-                            var j = 0;
-                            plugins.each(function (log) {
-                                j = j + 1;
-                                if (log.attributes.path != "js/saiku/plugins/I18n/plugin.js") {
-                                    jQuery.ajax({
-                                        async: false,
-                                        type: 'GET',
-                                        url: log.attributes.path,
-                                        data: null,
-                                        success: function () {
-                                            if (j == i) {
-                                                if (Settings.CSS != undefined) {
-                                                    Saiku.loadCSS(Settings.CSS, null)
-                                                }
-                                                Saiku.session = new Session({}, {
-                                                    username: Settings.USERNAME,
-                                                    password: Settings.PASSWORD
-                                                });
-
-                                                Saiku.toolbar = new Toolbar();
-                                            }
-                                        },
-                                        dataType: 'script'
-                                    });
-                                }
-                                else {
-                                    if (j == i) {
-
-                                        if (Settings.CSS != undefined) {
-                                            Saiku.loadCSS(Settings.CSS, null)
-                                        }
-                                        Saiku.session = new Session({}, {
-                                            username: Settings.USERNAME,
-                                            password: Settings.PASSWORD
-                                        });
-
-                                        Saiku.toolbar = new Toolbar();
-
-                                    }
-                                }
-                            });
-
-                        }
-                    });
-                }
-            });
-        });
-}
-/**
  * Dynamically load plugins!
+ *
  * @type {PluginCollection}
  */
+if (!Settings.BIPLUGIN) {
+    $(document).ready(function() {
+        var plugins = new PluginCollection();
 
+        plugins.fetch({
+            success: function() {
+                var settingsoverride = new SettingsOverrideCollection();
+
+                settingsoverride.fetch({
+                    success: function() {
+                        var i = plugins.size();
+                        var j = 0;
+
+                        plugins.each(function(log) {
+                            j = j + 1;
+
+                            if (log.attributes.path !== 'js/saiku/plugins/I18n/plugin.js') {
+                                jQuery.ajax({
+                                    async: false,
+                                    type: 'GET',
+                                    url: log.attributes.path,
+                                    data: null,
+                                    success: function() {
+                                        if (j === i) {
+                                            var k = settingsoverride.size();
+                                            var l = 0;
+
+                                            settingsoverride.each(function(log) {
+                                                l = l + 1;
+
+                                                for (var key in log.attributes) {
+                                                    if (log.attributes.hasOwnProperty(key)) {
+                                                        Settings[key] = log.attributes[key];
+                                                    }
+                                                }
+
+                                                if (Settings.CSS !== undefined) {
+                                                    Saiku.loadCSS(Settings.CSS, null);
+                                                }
+
+                                                if (k === l) {
+                                                    Saiku.session = new Session({}, {
+                                                        username: Settings.USERNAME,
+                                                        password: Settings.PASSWORD
+                                                    });
+
+                                                    Saiku.toolbar = new Toolbar();
+                                                }
+                                            });
+                                        }
+                                    },
+                                    dataType: 'script'
+                                });
+                            }
+                            else {
+                                if (j === i) {
+                                    var k = settingsoverride.size();
+                                    var l = 0;
+
+                                    settingsoverride.each(function(log) {
+                                        l = l + 1;
+
+                                        for (var key in log.attributes) {
+                                            if (log.attributes.hasOwnProperty(key)) {
+                                                Settings[key] = log.attributes[key];
+                                            }
+                                        }
+
+                                        if (Settings.CSS !== undefined) {
+                                            Saiku.loadCSS(Settings.CSS, null);
+                                        }
+
+                                        if (k === l) {
+                                            Saiku.session = new Session({}, {
+                                                username: Settings.USERNAME,
+                                                password: Settings.PASSWORD
+                                            });
+
+                                            Saiku.toolbar = new Toolbar();
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    },
+                    error: function() {
+                        var i = plugins.size();
+                        var j = 0;
+
+                        plugins.each(function(log) {
+                            j = j + 1;
+
+                            if (log.attributes.path !== 'js/saiku/plugins/I18n/plugin.js') {
+                                jQuery.ajax({
+                                    async: false,
+                                    type: 'GET',
+                                    url: log.attributes.path,
+                                    data: null,
+                                    success: function() {
+                                        if (j === i) {
+                                            if (Settings.CSS !== undefined) {
+                                                Saiku.loadCSS(Settings.CSS, null);
+                                            }
+
+                                            Saiku.session = new Session({}, {
+                                                username: Settings.USERNAME,
+                                                password: Settings.PASSWORD
+                                            });
+
+                                            Saiku.toolbar = new Toolbar();
+                                        }
+                                    },
+                                    dataType: 'script'
+                                });
+                            }
+                            else {
+                                if (j === i) {
+                                    if (Settings.CSS !== undefined) {
+                                        Saiku.loadCSS(Settings.CSS, null);
+                                    }
+
+                                    Saiku.session = new Session({}, {
+                                        username: Settings.USERNAME,
+                                        password: Settings.PASSWORD
+                                    });
+
+                                    Saiku.toolbar = new Toolbar();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+}
 
 var SaikuTimeLogger = function(element) {
     this._element = $(element);
@@ -438,15 +523,19 @@ var SaikuTimeLogger = function(element) {
 
 SaikuTimeLogger.prototype.log = function(eventname) {
     var time = (new Date()).getTime();
+
     if (!eventname) {
-        eventname = "Unknown";
+        eventname = 'Unknown';
     }
+
     if (this._timestamps.length > 0) {
         var lastTime = this._timestamps[this._timestamps.length -1];
+
         if ((time - lastTime) > 1) {
-            this._element.append( "<div>" + (time - lastTime) + " ms " + eventname + '  (previous: ' + this._events[this._events.length -1]  + " )</div>");
+            this._element.append('<div>' + (time - lastTime) + ' ms ' + eventname + ' (previous: ' + this._events[this._events.length -1]  + ' )</div>');
         }
     }
+
     this._timestamps.push(time);
     this._events.push(eventname);
 };
