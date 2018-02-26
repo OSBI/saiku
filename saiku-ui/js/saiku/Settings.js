635bb6f696c99d9e054c560027c399e4859fdf3e
brenopolanski <breno.polanski@gmail.com>
2018-02-26 15:35:47 -0300

    Update Settings.js

--- saiku-ui/js/saiku/Settings.js
+++ saiku-ui/js/saiku/Settings.js
@@ -14,17 +14,17 @@
  *   limitations under the License.
  */
 
-// If you would like to add more properties to Settings, Add in "Settings.yaml"
+// If you would like to add more properties to Settings, add in "Settings.yaml"
 var Settings = {};
 
 $.ajax({
     async: false, // do not change to true
     type: 'GET',
     url: 'js/saiku/Settings.yaml',
-    success: function(data) {
+    success: function(props) {
         // JavaScript YAML parser
         // link: https://github.com/nodeca/js-yaml
-        Settings = jsyaml.load(data);
+        Settings = jsyaml.load(props);
     }
 });
 
@@ -35,22 +35,24 @@ Settings.BASE_URL = window.location.origin;
  */
 Settings.GET = function () {
     var qs = document.location.search;
-    qs = qs.split("+").join(" ");
-    var params = {},
-        tokens,
-        re = /[?&]?([^=]+)=([^&]*)/g;
+    var params = {};
+    var tokens;
+    var re = /[?&]?([^=]+)=([^&]*)/g;
 
+    qs = qs.split('+').join(' ');
     tokens = re.exec(qs);
+
     while (tokens) {
         var value = decodeURIComponent(tokens[2]);
-        if (! isNaN(value)) value = parseInt(value);
-        if (value === "true") value = true;
-        if (value === "false") value = false;
-        if(decodeURIComponent(tokens[1].toUpperCase()).substring(0,5)==="PARAM"){
-          params["PARAM"+decodeURIComponent(tokens[1]).substring(5,decodeURIComponent(tokens[1]).length)] = value;
+
+        if (!isNaN(value)) value = parseInt(value);
+        if (value === 'true') value = true;
+        if (value === 'false') value = false;
+        if (decodeURIComponent(tokens[1].toUpperCase()).substring(0,5) === 'PARAM') {
+            params['PARAM' + decodeURIComponent(tokens[1]).substring(5, decodeURIComponent(tokens[1]).length)] = value;
         }
-        else{
-          params[decodeURIComponent(tokens[1]).toUpperCase()] = value;
+        else {
+            params[decodeURIComponent(tokens[1]).toUpperCase()] = value;
         }
 
         tokens = re.exec(qs);
@@ -62,22 +64,23 @@ _.extend(Settings, Settings.GET);
 
 Settings.PARAMS = (function() {
     var p = {};
+
     for (var key in Settings) {
-        if (key.match("^PARAM")=="PARAM") {
+        if (key.match('^PARAM') === 'PARAM') {
             p[key] = Settings[key];
         }
     }
+
     return p;
 }());
 
 Settings.REST_URL = Settings.TOMCAT_WEBAPP + Settings.REST_MOUNT_POINT;
 
 // lets assume we dont need a min width/height for table mode
-if (Settings.MODE == "table") {
+if (Settings.MODE === 'table') {
     Settings.DIMENSION_PREFETCH = false;
-    $('body, html').css('min-height',0);
-    $('body, html').css('min-width',0);
-
+    $('body, html').css('min-height', 0);
+    $('body, html').css('min-width', 0);
 }
 if (Settings.BIPLUGIN5) {
     Settings.BIPLUGIN = true;
@@ -86,39 +89,40 @@ if (Settings.BIPLUGIN5) {
 Settings.INITIAL_QUERY = false;
 if (document.location.hash) {
     var hash = document.location.hash;
-    if (hash.length > 11 && hash.substring(1, 11) == "query/open") {
+
+    if (hash.length > 11 && hash.substring(1, 11) === 'query/open') {
         Settings.INITIAL_QUERY = true;
     }
 }
 
 Settings.MONDRIAN_LOCALES = {
-    "English": "en_US",
-    "Dutch": "nl_BE",
-    "French": "fr_FR"
+    'English': 'en_US',
+    'Dutch'  : 'nl_BE',
+    'French' : 'fr_FR'
 };
 
 /**
  * < IE9 doesn't support Array.indexOf
  */
-if (!Array.prototype.indexOf)
-{
-  Array.prototype.indexOf = function(elt /*, from*/)
-  {
-    var len = this.length >>> 0;
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(elt /*, from*/) {
+        var len = this.length >>> 0;
+        var from = Number(arguments[1]) || 0;
 
-    var from = Number(arguments[1]) || 0;
-    from = (from < 0) ? Math.ceil(from) : Math.floor(from);
-    if (from < 0)
-      from += len;
+        from = (from < 0) ? Math.ceil(from) : Math.floor(from);
 
-    for (; from < len; from++)
-    {
-      if (from in this &&
-          this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
+        if (from < 0) {
+            from += len;
+        }
+
+        for (; from < len; from++) {
+            if (from in this && this[from] === elt) {
+                return from;
+            }
+        }
+
+        return -1;
+    };
 }
 
 /**
@@ -151,27 +155,26 @@ if ($.blockUI) {
     $.blockUI.defaults.fadeOut = 0;
     $.blockUI.defaults.fadeIn = 0;
     $.blockUI.defaults.ignoreIfBlocked = false;
-
 }
 
-if (window.location.hostname && (window.location.hostname == "try.meteorite.bi" )) {
-    Settings.USERNAME = "admin";
-    Settings.PASSWORD = "admin";
+if (window.location.hostname && (window.location.hostname === 'try.meteorite.bi')) {
+    Settings.USERNAME = 'demo';
+    Settings.PASSWORD = 'demo';
     Settings.DEMO = true;
     Settings.UPGRADE = false;
 }
 
 var isIE = (function(){
-    var undef, v = 3;
-
+    var v = 3;
     var dav = navigator.appVersion;
 
-    if(dav.indexOf('MSIE') != -1) {
-        v  = parseFloat(dav.split('MSIE ')[1]);
-        return v> 4 ? v : false;
-    }
-    return false;
+    if (dav.indexOf('MSIE') !== -1) {
+        v = parseFloat(dav.split('MSIE ')[1]);
 
+        return v > 4 ? v : false;
+    }
+
+    return false;
 }());
 
 var isFF = (function(userAgent) {
@@ -181,12 +184,17 @@ var isFF = (function(userAgent) {
 }(navigator.userAgent));
 
 var isMobile = (function(userAgent) {
-  'use strict';
+    'use strict';
 
-  return !!userAgent.match(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i);
+    return !!userAgent.match(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i);
 }(navigator.userAgent));
 
 /**
  * Extend settings with charts colors
  */
- Settings.CHART_COLORS = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
+Settings.CHART_COLORS = [
+    '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+    '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+    '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+    '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'
+];
