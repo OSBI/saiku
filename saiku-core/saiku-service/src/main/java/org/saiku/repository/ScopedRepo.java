f87b5384439c2b385778722719b7208ad54fc2a0
Tom Barber <tom.barber@jpl.nasa.gov>
2018-03-01 23:14:51 +0000

    fix merge

--- saiku-core/saiku-service/src/main/java/org/saiku/repository/ScopedRepo.java
+++ saiku-core/saiku-service/src/main/java/org/saiku/repository/ScopedRepo.java
@@ -1,13 +1,10 @@
 package org.saiku.repository;
 
-<<<<<<< HEAD
-=======
 import java.io.Serializable;
 
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
->>>>>>> tag-3.16.1
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.web.session.HttpSessionCreatedEvent;
 
@@ -19,32 +16,7 @@ import javax.servlet.http.HttpSession;
  * Created by bugg on 06/05/16.
  */
 public class ScopedRepo implements ApplicationListener<HttpSessionCreatedEvent>, Serializable {
-<<<<<<< HEAD
-  private static final long serialVersionUID = -8379337850087195764L;
-  private HttpSession httpSession;
 
-  public ScopedRepo() {
-
-  }
-
-  public void onApplicationEvent(HttpSessionCreatedEvent sessionEvent) {
-    /*
-     * In our previous implementation we just fetched a session from the
-     * RequestContextHolder, but in a SpringSecurity + Hazelcast scenario we need a
-     * Hazelcast enabled session which is injected just after the spring security
-     * session is created.
-     */
-    this.setSession(sessionEvent.getSession());
-  }
-
-  public void setSession(HttpSession httpSession) {
-    this.httpSession = httpSession;
-  }
-
-  public HttpSession getSession() {
-    return this.httpSession;
-  }
-=======
     static final long serialVersionUID = 1L;
 
     private transient HttpSession httpSession;
@@ -69,5 +41,4 @@ public class ScopedRepo implements ApplicationListener<HttpSessionCreatedEvent>,
     public HttpSession getSession(){
         return this.httpSession;
     }
->>>>>>> tag-3.16.1
 }
