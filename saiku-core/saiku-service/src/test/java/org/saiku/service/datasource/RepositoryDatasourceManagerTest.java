32fcff73c2aa067c42687a6b8580b48136c40f24
Matt Kynaston <matt@claritum.com>
2019-01-30 14:50:06 +0000

    #683 Added ability to set datasource and connection processors to RepositoryDatasourceManager

--- saiku-core/saiku-service/src/test/java/org/saiku/service/datasource/RepositoryDatasourceManagerTest.java
+++ saiku-core/saiku-service/src/test/java/org/saiku/service/datasource/RepositoryDatasourceManagerTest.java
@@ -4,8 +4,11 @@ import java.util.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import org.saiku.datasources.connection.ISaikuConnection;
 import org.saiku.datasources.datasource.SaikuDatasource;
 import org.saiku.repository.ScopedRepo;
+
+import org.springframework.security.web.session.HttpSessionCreatedEvent;
 
 public class RepositoryDatasourceManagerTest {
     private static final String JACKRABBIT = "jackrabbit";
@@ -98,6 +101,114 @@ public class RepositoryDatasourceManagerTest {
         };
 
         rdManager.addDatasource(ds);
+    }
+
+    @Test
+    public void testDatasourceProcessorAdded() throws Exception {
+        MockConnectionManager cManager = new MockConnectionManager();
+        MockRepositoryManager rManager = new MockRepositoryManager();
+
+        // Configuring session attributes
+        Map<String, Object> session = new HashMap<>();
+        session.put(RepositoryDatasourceManager.ORBIS_WORKSPACE_DIR, "workspace");
+
+        rdManager.setConnectionManager(cManager);
+        rdManager.setRepositoryManager(rManager);
+        rdManager.setType(CLASSPATH);
+        rdManager.setWorkspaces("true");
+        rdManager.setSessionRegistry(createScopedRepo(session));
+        rdManager.setDatadir("c:\\temp\\saikurepo");
+        rdManager.setExternalPropertiesFile("/does/not/exist");
+        rdManager.setDatasourceProcessor("org.example.DatasourceProcessor");
+
+        SaikuDatasource ds = new SaikuDatasource() {
+            @Override
+            public Type getType() {
+                return Type.OLAP;
+            }
+
+            @Override
+            public String getName() {
+                return "MOCK_DATA_MDYYYY";
+            }
+
+            @Override
+            public Properties getProperties() {
+                Properties props = new Properties();
+                props.setProperty("driver", "mondrian.olap4j.MondrianOlap4jDriver");
+                props.setProperty("location", "jdbc:mondrian:Jdbc=jdbc:calcite:model=c://temp/saikurepo/datasources/B_MOCK_DATA_MDYYYY-csv.json;Catalog=mondrian://datasources/B_MOCK_DATA_MDYYYY.xml;JdbcDrivers=org.apache.calcite.jdbc.Driver;");
+                props.setProperty("username", "bruno");
+                props.setProperty("password", "bruno");
+                props.setProperty("id", "b5ef4927-63e3-4d9c-b7dc-905fff8841f8");
+                props.setProperty("security.enabled", "false");
+                props.setProperty("type", "OLAP");
+                props.setProperty("csv", "true");
+
+                return props;
+            }
+        };
+
+        rdManager.addDatasource(ds);
+
+        rdManager.onApplicationEvent(new HttpSessionCreatedEvent(new MockHttpSession(session)));
+
+        Properties actual = rdManager.getDatasource("MOCK_DATA_MDYYYY").getProperties();
+        assertEquals("org.example.DatasourceProcessor", actual.getProperty(ISaikuConnection.DATASOURCE_PROCESSORS));
+    }
+
+
+
+    @Test
+    public void testConnectionProcessorAdded() throws Exception {
+        MockConnectionManager cManager = new MockConnectionManager();
+        MockRepositoryManager rManager = new MockRepositoryManager();
+
+        // Configuring session attributes
+        Map<String, Object> session = new HashMap<>();
+        session.put(RepositoryDatasourceManager.ORBIS_WORKSPACE_DIR, "workspace");
+
+        rdManager.setConnectionManager(cManager);
+        rdManager.setRepositoryManager(rManager);
+        rdManager.setType(CLASSPATH);
+        rdManager.setWorkspaces("true");
+        rdManager.setSessionRegistry(createScopedRepo(session));
+        rdManager.setDatadir("c:\\temp\\saikurepo");
+        rdManager.setExternalPropertiesFile("/does/not/exist");
+        rdManager.setConnectionProcessor("org.example.ConnectionProcessor");
+
+        SaikuDatasource ds = new SaikuDatasource() {
+            @Override
+            public Type getType() {
+                return Type.OLAP;
+            }
+
+            @Override
+            public String getName() {
+                return "MOCK_DATA_MDYYYY";
+            }
+
+            @Override
+            public Properties getProperties() {
+                Properties props = new Properties();
+                props.setProperty("driver", "mondrian.olap4j.MondrianOlap4jDriver");
+                props.setProperty("location", "jdbc:mondrian:Jdbc=jdbc:calcite:model=c://temp/saikurepo/datasources/B_MOCK_DATA_MDYYYY-csv.json;Catalog=mondrian://datasources/B_MOCK_DATA_MDYYYY.xml;JdbcDrivers=org.apache.calcite.jdbc.Driver;");
+                props.setProperty("username", "bruno");
+                props.setProperty("password", "bruno");
+                props.setProperty("id", "b5ef4927-63e3-4d9c-b7dc-905fff8841f8");
+                props.setProperty("security.enabled", "false");
+                props.setProperty("type", "OLAP");
+                props.setProperty("csv", "true");
+
+                return props;
+            }
+        };
+
+        rdManager.addDatasource(ds);
+
+        rdManager.onApplicationEvent(new HttpSessionCreatedEvent(new MockHttpSession(session)));
+
+        Properties actual = rdManager.getDatasource("MOCK_DATA_MDYYYY").getProperties();
+        assertEquals("org.example.ConnectionProcessor", actual.getProperty(ISaikuConnection.CONNECTION_PROCESSORS));
     }
 
     private ScopedRepo createScopedRepo(Map<String, Object> sessionAttributes) {
